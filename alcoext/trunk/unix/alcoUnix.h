fbf22dcfb296495c77ab4a1fc32916275ce8e719
neoxed <neoxed@8dc05064-dfdd-11de-a668-6fd11605f5ce>
2005-06-27 21:18:24 +0000

    Updated header checks for autoconf/autoheader.

--- alcoext/trunk/unix/alcoUnix.h
+++ alcoext/trunk/unix/alcoUnix.h
@@ -23,14 +23,31 @@
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
+#include <sys/file.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 #ifdef HAVE_INTTYPES_H
 #include <inttypes.h>
 #endif /* HAVE_INTTYPES_H */
 
+#ifdef HAVE_LIMITS_H
+#include <limits.h>
+#endif /* HAVE_LIMITS_H */
+
 #ifdef HAVE_STDINT_H
 #include <stdint.h>
 #endif /* HAVE_STDINT_H */
+
+#ifdef HAVE_TIME_H
+#include <time.h>
+#endif /* HAVE_TIME_H */
+
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif /* HAVE_UNISTD_H */
 
 #ifdef HAVE_SYS_PARAM_H
 #include <sys/param.h>
@@ -52,10 +69,9 @@
 #include <sys/vfs.h>
 #endif /* HAVE_SYS_VFS_H */
 
+#ifdef HAVE_SYS_TIME_H
 #include <sys/time.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
+#endif /* HAVE_SYS_TIME_H */
 
 #include "alcoUnixGlFtpd.h"
 
