c9fe2a7e54d90eb0330dfc6c00f0487264d9a381
neoxed <neoxed@8dc05064-dfdd-11de-a668-6fd11605f5ce>
2005-09-23 21:10:53 +0000

    Commented SQL columns (to explain their purpose).

--- alcobot/trunk/modules/generic/pretimes/pretimes.sql
+++ alcobot/trunk/modules/generic/pretimes/pretimes.sql
@@ -4,15 +4,15 @@
 
 CREATE TABLE `pretimes` (
   `id`       int unsigned      NOT NULL auto_increment,
-  `pretime`  int(10)           NOT NULL default '0',
-  `section`  varchar(20)       NOT NULL default '',
-  `release`  varchar(255)      NOT NULL default '',
-  `files`    smallint unsigned NOT NULL default '0',
-  `kbytes`   int unsigned      NOT NULL default '0',
-  `disks`    tinyint unsigned  NOT NULL default '0',
-  `nuked`    tinyint(1)        NOT NULL default '0',
-  `nuketime` int(10)           NOT NULL default '0',
-  `reason`   varchar(255)      NOT NULL default '',
+  `pretime`  int(10)           NOT NULL default '0', -- Time stamp of pre.
+  `section`  varchar(20)       NOT NULL default '',  -- Section name.
+  `release`  varchar(255)      NOT NULL default '',  -- Release name.
+  `files`    smallint unsigned NOT NULL default '0', -- Number of files present.
+  `kbytes`   int unsigned      NOT NULL default '0', -- Size in kilobytes.
+  `disks`    tinyint unsigned  NOT NULL default '0', -- Number of disks (CDs/DVDs).
+  `nuked`    tinyint(1)        NOT NULL default '0', -- Indicates if the release is nuked.
+  `nuketime` int(10)           NOT NULL default '0', -- Time stamp of nuke.
+  `reason`   varchar(255)      NOT NULL default '',  -- Reason for nuke.
   PRIMARY KEY (`id`),
   UNIQUE KEY  (`release`)
 );
