8890654ecbdc4dbf033254337cfe0a9689406145
neoxed <neoxed@8dc05064-dfdd-11de-a668-6fd11605f5ce>
2005-09-25 17:16:54 +0000

    Allow the irc_user and password columns to be NULL.

--- alcobot/trunk/modules/generic/invite/invite.sql
+++ alcobot/trunk/modules/generic/invite/invite.sql
@@ -4,9 +4,9 @@
 
 CREATE TABLE `invite_users` (
   `ftp_user` varchar(255) NOT NULL default '',  -- FTP user name.
-  `irc_user` varchar(255) NOT NULL default '',  -- IRC user name.
+  `irc_user` varchar(255) default NULL,         -- IRC user name.
   `online`   tinyint(1)   NOT NULL default '0', -- Indicates if the user is on IRC.
-  `password` varchar(255) NOT NULL default '',  -- Password hash (PKCS #5 v2 based).
+  `password` varchar(255) default NULL,         -- Password hash (PKCS #5 v2 based).
   `time`     int(10)      NOT NULL default '0', -- Time stamp of last activity.
   PRIMARY KEY (`ftp_user`)
 );
