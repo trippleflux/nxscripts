710131d7af082d3239f1d377155711d499ededd4
neoxed <neoxed@8dc05064-dfdd-11de-a668-6fd11605f5ce>
2005-10-10 23:26:25 +0000

    Added asynchronous socket code for connecting and disconnecting.

--- alcobot/trunk/libs/libFtp.tcl
+++ alcobot/trunk/libs/libFtp.tcl
@@ -11,13 +11,31 @@
 # Abstract:
 #   Implements a FTP client library to interact with FTP servers.
 #
+#   Exported Procedures:
+#     FtpOpen        <host> <port> <user> <passwd> [secure]
+#     FtpClose       <handle>
+#     FtpGetError    <handle>
+#     FtpConnect     <handle>
+#     FtpDisconnect  <handle>
+#     FtpIsConnected <handle>
+#
 
 namespace eval ::alcoholicz {
     if {![info exists ftpNextHandle]} {
-        set ftpNextHandle 0
+        variable ftpNextHandle 0
     }
     catch {package require tls 1.5}
-    namespace export FtpOpen FtpClose
+    namespace export FtpOpen FtpClose FtpGetError \
+        FtpConnect FtpDisconnect FtpIsConnected
+}
+
+####
+# bgerror
+#
+# Display background errors.
+#
+proc bgerror {message} {
+    ::alcoholicz::LogDebug BgError $message
 }
 
 ####
@@ -36,7 +54,7 @@ proc ::alcoholicz::FtpAcquire {handle {handleVar "ftp"}} {
 ####
 # FtpOpen
 #
-# Create a new FTP client handle. This handle is used by every FTP procedure
+# Creates a new FTP client handle. This handle is used by every FTP procedure
 # and must be closed using FtpClose.
 #
 # Secure Options:
@@ -45,7 +63,7 @@ proc ::alcoholicz::FtpAcquire {handle {handleVar "ftp"}} {
 # ssl      - Explicit SSL connection (AUTH SSL).
 # tls      - Explicit TLS connection (AUTH TLS).
 #
-proc ::alcoholicz::FtpOpen {server port user passwd {secure "none"}} {
+proc ::alcoholicz::FtpOpen {host port user passwd {secure "none"}} {
     variable ftpNextHandle
 
     switch -- $secure {
@@ -64,11 +82,13 @@ proc ::alcoholicz::FtpOpen {server port user passwd {secure "none"}} {
     upvar [namespace current]::$handle ftp
 
     array set ftp [list \
-        server $user    \
+        host   $host    \
         port   $port    \
         user   $user    \
         passwd $passwd  \
         secure $secure  \
+        error  ""       \
+        sock   ""       \
     ]
 
     incr ftpNextHandle
@@ -84,4 +104,101 @@ proc ::alcoholicz::FtpClose {handle} {
     FtpAcquire $handle
     unset -nocomplain ftp
     return
+}
+
+####
+# FtpGetError
+#
+# Returns the last error message.
+#
+proc ::alcoholicz::FtpGetError {handle} {
+    FtpAcquire $handle
+    return $ftp(error)
+}
+
+####
+# FtpConnect
+#
+# Connect to the FTP server.
+#
+proc ::alcoholicz::FtpConnect {handle} {
+    FtpAcquire $handle
+
+    if {$ftp(sock) ne ""} {
+        error "ftp connection already open, disconnect first"
+    }
+
+    # Asynchronous sockets in Tcl are created immediately but may not be
+    # connected yet. The writable channel event callback is executed when
+    # the socket is connected or if the connection failed.
+    set ftp(sock) [socket -async $ftp(host) $ftp(port)]
+
+    fileevent $ftp(sock) writable [list [namespace current]::FtpVerify $handle]
+    return
+}
+
+####
+# FtpVerify
+#
+# Verifies the connection's state and begins the SSL negotiation for
+# FTP servers using implicit SSL.
+#
+proc ::alcoholicz::FtpVerify {handle} {
+    upvar [namespace current]::$handle ftp
+    if {![info exists ftp]} {
+        LogDebug FtpVerify "Handle \"$handle\" closed before connection succeeded."
+        return
+    }
+
+    # Disable the writeable channel event.
+    fileevent $ftp(sock) writable {}
+
+    set ftp(error) [fconfigure $ftp(sock) -error]
+    if {$ftp(error) ne ""} {
+        LogDebug FtpVerify "Unable to connect to $ftp(host):$ftp(port) ($handle): $ftp(error)"
+
+        # The socket must be closed and set to an empty string
+        # in order for FtpIsConnected to return false.
+        catch {close $ftp(sock)}
+        set ftp(sock) ""
+        return
+    }
+
+    set peer [fconfigure $ftp(sock) -peername]
+    LogDebug FtpVerify "Connected to [lindex $peer 0]:[lindex $peer 2] ($handle)."
+
+    # Perform SSL negotiation for FTP servers using implicit SSL.
+    if {$ftp(secure) eq "implicit"} {
+        set ftp(sock) [tls::import $ftp(sock) -require 0 -ssl2 1 -ssl3 1 -tls1 1]
+    }
+
+    # Set socket options and event handlers.
+    fconfigure $ftp(sock) -buffering line -blocking 0 -translation {auto crlf}
+    # TODO: State handler.
+    #fileevent $ftp(sock) readable [list [namespace current]::FtpHandler $handle]
+    return
+}
+
+####
+# FtpDisconnect
+#
+# Disconnects from the FTP server.
+#
+proc ::alcoholicz::FtpDisconnect {handle} {
+    FtpAcquire $handle
+
+    # TODO: Send QUIT
+    catch {close $ftp(sock)}
+    set ftp(sock) ""
+    return
+}
+
+####
+# FtpIsConnected
+#
+# Checks if the current handle is connected.
+#
+proc ::alcoholicz::FtpIsConnected {handle} {
+    FtpAcquire $handle
+    return [expr {$ftp(sock) ne ""}]
 }
