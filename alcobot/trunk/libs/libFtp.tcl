fe631bf2cb23daec3f0badee29b1c012e7526c69
neoxed <neoxed@8dc05064-dfdd-11de-a668-6fd11605f5ce>
2005-10-12 01:19:37 +0000

    FTP client library:  * Completed the FIFO based queueing system.  * Finished the event handler and login sequence.  * Send a QUIT command before terminating the socket.

--- alcobot/trunk/libs/libFtp.tcl
+++ alcobot/trunk/libs/libFtp.tcl
@@ -36,7 +36,7 @@ namespace eval ::alcoholicz {
 # Validate and acquire a FTP handle. This procedure is for internal use only,
 # hence why it is not exported.
 #
-proc ::alcoholicz::FtpAcquire {handle {handleVar "ftp"}} {
+proc ::alcoholicz::FtpAcquire {handle handleVar} {
     if {![regexp -- {ftp\d+} $handle] || ![array exists [namespace current]::$handle]} {
         error "invalid ftp handle \"$handle\""
     }
@@ -64,7 +64,7 @@ proc ::alcoholicz::FtpOpen {host port user passwd args} {
             switch -- $value {
                 none {}
                 implicit - ssl - tls {
-                    # Make sure the TLS package is present (http://tls.sourceforge.net).
+                    # Make sure the TLS package (http://tls.sf.net) is present.
                     package present tls
                 }
                 default {
@@ -86,13 +86,13 @@ proc ::alcoholicz::FtpOpen {host port user passwd args} {
     #
     # FTP Handle Contents
     #
-    # ftp(host)   - Remote servet host.
-    # ftp(port)   - Remote servet port.
+    # ftp(host)   - Remote server host.
+    # ftp(port)   - Remote server port.
     # ftp(user)   - Client user name.
     # ftp(passwd) - Client password.
-    # ftp(secure) -
-    # ftp(event)  - Event handler state.
+    # ftp(secure) - Connect securely, using SSL or TLS.
     # ftp(error)  - Last error message.
+    # ftp(queue)  - Event queue (FIFO).
     # ftp(sock)   - Socket channel.
     # ftp(status) - Connection status (0=disconnected, 1=connecting, 2=connected).
     #
@@ -103,7 +103,7 @@ proc ::alcoholicz::FtpOpen {host port user passwd args} {
         passwd $passwd  \
         secure $secure  \
         error  ""       \
-        event  ""       \
+        queue  [list]   \
         sock   ""       \
         status 0        \
     ]
@@ -118,7 +118,7 @@ proc ::alcoholicz::FtpOpen {host port user passwd args} {
 # Closes and invalidates the specified handle.
 #
 proc ::alcoholicz::FtpClose {handle} {
-    FtpAcquire $handle
+    FtpAcquire $handle ftp
     FtpShutdown $handle
     unset -nocomplain ftp
     return
@@ -130,7 +130,7 @@ proc ::alcoholicz::FtpClose {handle} {
 # Returns the last error message.
 #
 proc ::alcoholicz::FtpGetError {handle} {
-    FtpAcquire $handle
+    FtpAcquire $handle ftp
     return $ftp(error)
 }
 
@@ -140,7 +140,7 @@ proc ::alcoholicz::FtpGetError {handle} {
 # Returns the connection status.
 #
 proc ::alcoholicz::FtpGetStatus {handle} {
-    FtpAcquire $handle
+    FtpAcquire $handle ftp
     return $ftp(status)
 }
 
@@ -150,11 +150,12 @@ proc ::alcoholicz::FtpGetStatus {handle} {
 # Connect to the FTP server.
 #
 proc ::alcoholicz::FtpConnect {handle} {
-    FtpAcquire $handle
+    FtpAcquire $handle ftp
 
     if {$ftp(sock) ne ""} {
         error "ftp connection open, disconnect first"
     }
+    set ftp(error) ""
     set ftp(status) 1
 
     # Asynchronous sockets in Tcl are created immediately but may not be
@@ -172,7 +173,7 @@ proc ::alcoholicz::FtpConnect {handle} {
 # Disconnects from the FTP server.
 #
 proc ::alcoholicz::FtpDisconnect {handle} {
-    FtpAcquire $handle
+    FtpAcquire $handle ftp
     FtpShutdown $handle
     return
 }
@@ -180,12 +181,51 @@ proc ::alcoholicz::FtpDisconnect {handle} {
 ####
 # FtpCommand
 #
-# Sends a command to the FTP server.
+# Sends a command to the FTP server. The server's response can be retrieved
+# by specifying a callback, since this library operates asynchronously.
+# For example:
+#
+# proc SiteWhoCallback {buffer} {
+#     foreach {code text} $buffer {
+#         putlog "$code: $text"
+#     }
+# }
+#
+# set handle [FtpOpen localhost 21 user pass]
+# FtpConnect $handle
+# FtpCommand $handle "SITE WHO" SiteWhoCallback
+# FtpClose $handle
 #
 proc ::alcoholicz::FtpCommand {handle command {callback ""}} {
-    FtpAcquire $handle
+    FtpAcquire $handle ftp
 
-    # TODO: Queue command.
+    if {$ftp(status) != 2} {
+        error "not connected"
+    }
+    lappend ftp(queue) [list quote $command $callback]
+
+    # If there's only event in queue, invoke the handler directly.
+    if {[llength $ftp(queue)] == 1} {
+        FtpHandler $handle 1
+    }
+    return
+}
+
+####
+# FtpSend
+#
+# Sends a command to the FTP control channel.
+#
+proc ::alcoholicz::FtpSend {handle command} {
+    upvar [namespace current]::$handle ftp
+
+    if {[info exists ftp]} {
+        if {[catch {puts $ftp(sock) $command} message]} {
+            FtpShutdown $handle "unable to send command - $message"
+        } else {
+            catch {flush $ftp(sock)}
+        }
+    }
     return
 }
 
@@ -194,12 +234,24 @@ proc ::alcoholicz::FtpCommand {handle command {callback ""}} {
 #
 # Shuts down the FTP connection.
 #
-proc ::alcoholicz::FtpShutdown {handle} {
+proc ::alcoholicz::FtpShutdown {handle {error ""}} {
     upvar [namespace current]::$handle ftp
+    LogDebug FtpShutdown "Connection closed ($handle): $error"
+
     if {[info exists ftp]} {
+        # Remove channel events before closing the channel.
+        catch {fileevent $ftp(sock) readable {}}
+        catch {fileevent $ftp(sock) writable {}}
+
+        # Send the QUIT command and terminate the socket.
+        catch {puts $ftp(sock) "QUIT"}
+        catch {flush $ftp(sock)}
         catch {close $ftp(sock)}
         set ftp(sock) ""
+
+        # Update connection status and error message.
         set ftp(status) 0
+        if {$error ne ""} {set ftp(error) $error}
     }
 }
 
@@ -216,26 +268,32 @@ proc ::alcoholicz::FtpVerify {handle} {
         return
     }
 
-    # Disable the writeable channel event.
+    # Disable the writable channel event.
     fileevent $ftp(sock) writable {}
 
-    set ftp(error) [fconfigure $ftp(sock) -error]
-    if {$ftp(error) ne ""} {
-        LogDebug FtpVerify "Unable to connect to $ftp(host):$ftp(port) ($handle): $ftp(error)"
-        FtpShutdown $handle
+    set message [fconfigure $ftp(sock) -error]
+    if {$message ne ""} {
+        FtpShutdown $handle "unable to connect - $message"
         return
     }
-    set ftp(status) 2
 
     set peer [fconfigure $ftp(sock) -peername]
     LogDebug FtpVerify "Connected to [lindex $peer 0]:[lindex $peer 2] ($handle)."
 
     # Perform SSL negotiation for FTP servers using implicit SSL.
-    if {$ftp(secure) eq "implicit"} {
-        set ftp(sock) [tls::import $ftp(sock) -require 0 -ssl2 1 -ssl3 1 -tls1 1]
+    if {$ftp(secure) eq "implicit" && [catch {tls::import $ftp(sock) -ssl2 1 -ssl3 1 -tls1 1} message]} {
+        FtpShutdown $handle "SSL negotiation failed - $message"
+        return
     }
 
-    # Set socket options and event handlers.
+    # Initialise event queue.
+    if {$ftp(secure) eq "ssl" || $ftp(secure) eq "tls"} {
+        set ftp(queue) auth
+    } else {
+        set ftp(queue) user
+    }
+
+    # Set channel options and event handlers.
     fconfigure $ftp(sock) -buffering line -blocking 0 -translation {auto crlf}
     fileevent $ftp(sock) readable [list [namespace current]::FtpHandler $handle]
     return
@@ -246,14 +304,166 @@ proc ::alcoholicz::FtpVerify {handle} {
 #
 # FTP client event handler.
 #
-proc ::alcoholicz::FtpHandler {handle} {
+proc ::alcoholicz::FtpHandler {handle {direct 0}} {
     upvar [namespace current]::$handle ftp
     if {![info exists ftp]} {
         LogDebug FtpHandler "Handle \"$handle\" does not exist."
         return
     }
 
-    # TODO: everything
+    set replyCode 0
+    set replyBase 0
+    set buffer [list]
+    set message ""
 
+    if {[gets $ftp(sock) line] > 0} {
+        #
+        # Multi-line responses have a hyphen after the reply code for
+        # each line until the last line is reached. For example:
+        #
+        # 200-blah
+        # 200-blah
+        # 200-blah
+        # 200 Command successful.
+        #
+        if {[regexp -- {^([0-9]+)( |-)?(.*)$} $line result replyCode multiLine message]} {
+            lappend buffer $replyCode $message
+        } else {
+            LogDebug FtpHandler "Invalid server response \"$line\"."
+            set multiLine ""
+        }
+
+        #
+        # The "STAT -al" response differs substantially, all subsequent lines
+        # after the initial response do not have a reply code until the last line.
+        #
+        # 211-Status of .:
+        # drwxrwxrwx  22 user         group               0 Oct 07 03:49 .
+        # drwxrwxrwx   5 user         group               0 Apr 02 02:59 blah1
+        # drwxrwxrwx  25 user         group               0 Oct 11 00:00 blah2
+        # drwxrwxrwx  22 user         group               0 Sep 27 22:52 blah3
+        # drwxrwxrwx  37 user         group               0 Jun 06 03:39 blah4
+        # 211 End of Status
+        #
+        # Because of this, the line is appended to the response buffer
+        # regardless of whether or not it matches the regular expression.
+        #
+        while {$multiLine eq "-" && [gets $ftp(sock) line] > 0} {
+            regexp -- {^([0-9]+)( |-)?(.*)$} $line result replyCode multiLine replyText
+            lappend buffer $replyCode $replyText
+        }
+    } elseif {[eof $ftp(sock)]} {
+        # The remote server has closed the control connection.
+        FtpShutdown $handle "server closed connection"
+        return
+    } elseif {!$direct} {
+        # No response from the server. Return only if the handler was not
+        # invoked directly (i.e. not by a channel writable event).
+        return
+    }
+    LogDebug FtpHandler "Reply code \"$replyCode\" and message \"$message\" ($handle)."
+
+    #
+    # Variables:
+    # replyCode - Reply code (e.g. 200).
+    # replyBase - Base reply code (e.g. 2).
+    # buffer    - List of reply codes and text messages.
+    # message   - Text from the first line (lindex $buffer 1).
+    #
+    set replyBase [string index $replyCode 0]
+
+    while {[llength $ftp(queue)]} {
+        set nextEvent 0
+
+        # The first list element of an event must be its name, the
+        # remaining elements are optional and vary between event types.
+        set event [lindex $ftp(queue) 0]
+        set eventName [lindex $event 0]
+
+        # Pop the event from the queue.
+        set ftp(queue) [lrange $ftp(queue) 1 end]
+
+        LogDebug FtpHandler "Event: $eventName ($handle)"
+        switch -- $eventName {
+            auth {
+                # Receive hello response and send AUTH.
+                if {$replyBase == 2} {
+                    FtpSend $handle "AUTH [string toupper $ftp(secure)]"
+                    set ftp(queue) auth_sent
+                } else {
+                    FtpShutdown $handle "unable to login - $message"
+                    return
+                }
+            }
+            auth_sent {
+                # Receive AUTH response and send PBSZ.
+                if {$replyBase == 2} {
+                    if {[catch {tls::import $ftp(sock) -ssl2 1 -ssl3 1 -tls1 1} message]} {
+                        FtpShutdown $handle "SSL negotiation failed - $message"
+                        return
+                    }
+                    # Reset channel options, in case the TLS module changes them.
+                    fconfigure $ftp(sock) -buffering line -blocking 0 -translation {auto crlf}
+
+                    FtpSend $handle "PBSZ 0"
+                    set ftp(queue) user
+                } else {
+                    FtpShutdown $handle "unable to login - $message"
+                    return
+                }
+            }
+            user {
+                # Receive hello or PBSZ response and send USER.
+                if {$replyBase == 2} {
+                    FtpSend $handle "USER $ftp(user)"
+                    set ftp(queue) user_sent
+                } else {
+                    FtpShutdown $handle "unable to login - $message"
+                    return
+                }
+            }
+            user_sent {
+                # Receive USER response and send PASS.
+                if {$replyBase == 3} {
+                    FtpSend $handle "PASS $ftp(passwd)"
+                    set ftp(queue) pass_sent
+                } else {
+                    FtpShutdown $handle "unable to login - $message"
+                    return
+                }
+            }
+            pass_sent {
+                # Receive PASS response.
+                if {$replyBase == 2} {
+                    set ftp(status) 2
+                    set nextEvent 1
+                } else {
+                    FtpShutdown $handle "unable to login - $message"
+                    return
+                }
+            }
+            quote {
+                # Send command.
+                FtpSend $handle [lindex $event 1]
+                set ftp(queue) [linsert $ftp(queue) 0 [list quote_sent [lindex $event 2]]]
+            }
+            quote_sent {
+                # Receive command.
+                set callback [lindex $event 1]
+                if {$callback ne ""} {
+                    if {[catch {eval $callback [list $buffer]} message]} {
+                        LogDebug FtpHandler $message
+                    }
+                }
+                set nextEvent 1
+            }
+            default {
+                LogDebug FtpHandler "Invalid event name \"$eventName\"."
+            }
+        }
+
+        # Proceed to the next event?
+        if {!$nextEvent} {break}
+    }
     return
 }
