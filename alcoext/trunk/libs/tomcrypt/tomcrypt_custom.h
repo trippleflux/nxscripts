90126cbe1d5cb4627e245fa15c734fa4d8d11510
neoxed <neoxed@8dc05064-dfdd-11de-a668-6fd11605f5ce>
2005-11-28 02:48:21 +0000

    Modified the LTC configuration.

--- alcoext/trunk/libs/tomcrypt/tomcrypt_custom.h
+++ alcoext/trunk/libs/tomcrypt/tomcrypt_custom.h
@@ -1,173 +1,107 @@
 #ifndef TOMCRYPT_CUSTOM_H_
 #define TOMCRYPT_CUSTOM_H_
 
-/* macros for various libc functions you can change for embedded targets */
-#ifndef XMALLOC
-#define XMALLOC  malloc
-#endif
-#ifndef XREALLOC
-#define XREALLOC realloc
-#endif
-#ifndef XCALLOC
-#define XCALLOC  calloc
-#endif
-#ifndef XFREE
-#define XFREE    free
+#ifdef HAVE_CONFIG_H
+#   include "config.h"
 #endif
 
-#ifndef XMEMSET
-#define XMEMSET  memset
-#endif
-#ifndef XMEMCPY
-#define XMEMCPY  memcpy
-#endif
-#ifndef XMEMCMP
-#define XMEMCMP  memcmp
-#endif
+// Use Tcl's memory allocation functions.
+#include <tcl.h>
 
-#ifndef XCLOCK
-#define XCLOCK   clock
-#endif
-#ifndef XCLOCKS_PER_SEC
+//
+// Tcl_AttemptAlloc and Tcl_AttemptRealloc do not cause the interpreter to
+// panic if the requested memory allocation fails. Since LibTomCrypt checks
+// return value of XMALLOC/XREALLOC, the 'attempt' functions are better suited.
+//
+#ifdef TCL_MEM_DEBUG
+#   define XMALLOC(n)       ((void *)Tcl_AttemptDbCkalloc((n), __FILE__, __LINE__))
+#   define XREALLOC(p,n)    ((void *)Tcl_AttemptDbCkrealloc((char *)(p), (n), __FILE__, __LINE__))
+#   define XFREE(p)         Tcl_DbCkfree((char *)(p), __FILE__, __LINE__)
+#else
+#   define XMALLOC(n)       ((void *)Tcl_AttemptAlloc((n)))
+#   define XREALLOC(p,n)    ((void *)Tcl_AttemptRealloc((char *)(p),(n)))
+#   define XFREE(p)         Tcl_Free((char *)(p))
+#endif // TCL_MEM_DEBUG
+
+#define XCLOCK          clock
 #define XCLOCKS_PER_SEC CLOCKS_PER_SEC
+#define XMEMCMP         memcmp
+#define XMEMCPY         memcpy
+#define XMEMSET         memset
+#define XQSORT          qsort
+
+/* type of argument checking, 0=default, 1=fatal and 2=error+continue, 3=nothing */
+#ifdef DEBUG
+#   define ARGTYPE 1
+#else
+#   define ARGTYPE 3
 #endif
 
-#ifndef XQSORT
-#define XQSORT qsort
-#endif
+/* Disable self-test vector checking */
+#define LTC_NO_TEST
 
-/* Use small code where possible */
-/* #define LTC_SMALL_CODE */
+/* Disable all file related functions */
+#define LTC_NO_FILE
 
-/* Enable self-test test vector checking */
-#ifndef LTC_NO_TEST
-   #define LTC_TEST
-#endif
-
-/* clean the stack of functions which put private information on stack */
-/* #define LTC_CLEAN_STACK */
-
-/* disable all file related functions */
-/* #define LTC_NO_FILE */
-
-/* disable all forms of ASM */
-/* #define LTC_NO_ASM */
-
-/* disable FAST mode */
-/* #define LTC_NO_FAST */
-
-/* disable BSWAP on x86 */
-/* #define LTC_NO_BSWAP */
-
-/* ---> Symmetric Block Ciphers <--- */
-#ifndef LTC_NO_CIPHERS
-
+/* Symmetric Block Ciphers */
+#define ANUBIS
+#define ANUBIS_TWEAK
 #define BLOWFISH
+#define CAST5
+#define DES
+#define KHAZAD
+#define NOEKEON
 #define RC2
 #define RC5
 #define RC6
-#define SAFERP
 #define RIJNDAEL
-#define XTEA
-/* _TABLES tells it to use tables during setup, _SMALL means to use the smaller scheduled key format
- * (saves 4KB of ram), _ALL_TABLES enables all tables during setup */
-#define TWOFISH
-#ifndef LTC_NO_TABLES
-   #define TWOFISH_TABLES
-   /* #define TWOFISH_ALL_TABLES */
-#else
-   #define TWOFISH_SMALL
-#endif
-/* #define TWOFISH_SMALL */
-/* DES includes EDE triple-DES */
-#define DES
-#define CAST5
-#define NOEKEON
-#define SKIPJACK
 #define SAFER
-#define KHAZAD
-#define ANUBIS
-#define ANUBIS_TWEAK
+#define SAFERP
+#define SKIPJACK
+#define TWOFISH
+#define TWOFISH_TABLES
+#define XTEA
 
-#endif /* LTC_NO_CIPHERS */
-
-
-/* ---> Block Cipher Modes of Operation <--- */
-#ifndef LTC_NO_MODES
-
-#define CFB
-#define OFB
-#define ECB
+/* Block Cipher Modes of Operation */
 #define CBC
+#define CFB
 #define CTR
+#define ECB
+#define OFB
 
-#endif /* LTC_NO_MODES */
-
-/* ---> One-Way Hash Functions <--- */
-#ifndef LTC_NO_HASHES
-
-#define CHC_HASH
-#define WHIRLPOOL
-#define SHA512
-#define SHA384
-#define SHA256
-#define SHA224
-#define TIGER
-#define SHA1
-#define MD5
-#define MD4
+/* One-Way Hash Functions */
 #define MD2
+#define MD4
+#define MD5
 #define RIPEMD128
 #define RIPEMD160
+#define SHA1
+#define SHA224
+#define SHA256
+#define SHA384
+#define SHA512
+#define TIGER
+#define WHIRLPOOL
 
-#endif /* LTC_NO_HASHES */
-
-/* ---> MAC functions <--- */
-#ifndef LTC_NO_MACS
-
+/* MAC functions */
 #define HMAC
 #define OMAC
-#define PMAC
 #define PELICAN
+#define PMAC
 
 #if defined(PELICAN) && !defined(RIJNDAEL)
    #error Pelican-MAC requires RIJNDAEL
 #endif
 
-/* ---> Encrypt + Authenticate Modes <--- */
-
-#define EAX_MODE
-#if defined(EAX_MODE) && !(defined(CTR) && defined(OMAC))
-   #error EAX_MODE requires CTR and OMAC mode
-#endif
-
-#define OCB_MODE
-#define CCM_MODE
-
-#define GCM_MODE
-
-/* Use 64KiB tables */
-#ifndef LTC_NO_TABLES
-   #define GCM_TABLES
-#endif
-
-#endif /* LTC_NO_MACS */
-
 /* Various tidbits of modern neatoness */
 #define BASE64
 
-/* --> Pseudo Random Number Generators <--- */
-#ifndef LTC_NO_PRNGS
+/* Pseudo Random Number Generators */
 
-/* Yarrow */
 #define YARROW
 /* which descriptor of AES to use?  */
 /* 0 = rijndael_enc 1 = aes_enc, 2 = rijndael [full], 3 = aes [full] */
-#define YARROW_AES 0
-
-#if defined(YARROW) && !defined(CTR)
-   #error YARROW requires CTR chaining mode to be defined!
-#endif
+#define YARROW_AES 2
 
 /* a PRNG that simply reads from an available system source */
 #define SPRNG
@@ -182,7 +116,7 @@
 /* number of pools (4..32) can save a bit of ram by lowering the count */
 #define FORTUNA_POOLS 32
 
-/* Greg's SOBER128 PRNG ;-0 */
+/* Greg's SOBER128 PRNG */
 #define SOBER128
 
 /* the *nix style /dev/random device */
@@ -190,96 +124,10 @@
 /* try /dev/urandom before trying /dev/random */
 #define TRY_URANDOM_FIRST
 
-#endif /* LTC_NO_PRNGS */
-
-/* ---> math provider? <--- */
-#ifndef LTC_NO_MATH
-
-/* LibTomMath */
-/* #define LTM_DESC */
-
-/* TomsFastMath */
-/* #define TFM_DESC */
-
-#endif /* LTC_NO_MATH */
-
-/* ---> Public Key Crypto <--- */
-#ifndef LTC_NO_PK
-
-/* Include RSA support */
-#define MRSA
-
-/* Include Katja (a Rabin variant like RSA) */
-// #define MKAT
-
-/* Digital Signature Algorithm */
-#define MDSA
-
-/* ECC */
-#define MECC
-
-/* Timing Resistant? */
-/* #define LTC_ECC_TIMING_RESISTANT */
-
-#endif /* LTC_NO_PK */
-
 /* PKCS #1 (RSA) and #5 (Password Handling) stuff */
-#ifndef LTC_NO_PKCS
-
-#define PKCS_1
 #define PKCS_5
 
-/* Include ASN.1 DER (required by DSA/RSA) */
-#define LTC_DER
-
-#endif /* LTC_NO_PKCS */
-
-/* cleanup */
-
-#ifdef MECC
-/* Supported ECC Key Sizes */
-#ifndef LTC_NO_CURVES
-   #define ECC192
-   #define ECC224
-   #define ECC256
-   #define ECC384
-   #define ECC521
-#endif
-#endif
-
-#if defined(MECC) || defined(MRSA) || defined(MDSA) || defined(MKATJA)
-   /* Include the MPI functionality?  (required by the PK algorithms) */
-   #define MPI
-#endif
-
-#ifdef MRSA
-   #define PKCS_1
-#endif
-
-#if defined(LTC_DER) && !defined(MPI)
-   #error ASN.1 DER requires MPI functionality
-#endif
-
-#if (defined(MDSA) || defined(MRSA) || defined(MECC) || defined(MKATJA)) && !defined(LTC_DER)
-   #error PK requires ASN.1 DER functionality, make sure LTC_DER is enabled
-#endif
-
-/* THREAD management */
-
-#ifdef LTC_PTHREAD
-
-#include <pthread.h>
-
-#define LTC_MUTEX_GLOBAL(x)   pthread_mutex_t x = PTHREAD_MUTEX_INITIALIZER;
-#define LTC_MUTEX_PROTO(x)    extern pthread_mutex_t x;
-#define LTC_MUTEX_TYPE(x)     pthread_mutex_t x;
-#define LTC_MUTEX_INIT(x)     pthread_mutex_init(x, NULL);
-#define LTC_MUTEX_LOCK(x)     pthread_mutex_lock(x);
-#define LTC_MUTEX_UNLOCK(x)   pthread_mutex_unlock(x);
-
-#else
-
-/* default no functions */
+/* No thread management */
 #define LTC_MUTEX_GLOBAL(x)
 #define LTC_MUTEX_PROTO(x)
 #define LTC_MUTEX_TYPE(x)
@@ -288,11 +136,3 @@
 #define LTC_MUTEX_UNLOCK(x)
 
 #endif
-
-#endif
-
-
-
-/* $Source: /cvs/libtom/libtomcrypt/src/headers/tomcrypt_custom.h,v $ */
-/* $Revision: 1.34 $ */
-/* $Date: 2005/11/24 03:30:18 $ */
