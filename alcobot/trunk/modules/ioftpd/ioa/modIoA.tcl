21e14fa5a8d388fa863fcc6d2f2a73d750a7afec
neoxed <neoxed@8dc05064-dfdd-11de-a668-6fd11605f5ce>
2005-12-17 23:14:14 +0000

    Beginning work on a module for ioA support.

---
+++ alcobot/trunk/modules/ioftpd/ioa/modIoA.tcl
@@ -0,0 +1,35 @@
+#
+# AlcoBot - Alcoholicz site bot.
+# Copyright (c) 2005 Alcoholicz Scripting Team
+#
+# Module Name:
+#   ioA Module
+#
+# Author:
+#   neoxed (neoxed@gmail.com) Dec 17, 2005
+#
+# Abstract:
+#   Implements a module to interact with ioA's data files.
+#
+
+namespace eval ::alcoholicz::IoA {
+    namespace import -force ::alcoholicz::*
+}
+
+####
+# Load
+#
+# Module initialisation procedure, called when the module is loaded.
+#
+proc ::alcoholicz::IoA::Load {firstLoad} {
+    return
+}
+
+####
+# Unload
+#
+# Module finalisation procedure, called before the module is unloaded.
+#
+proc ::alcoholicz::IoA::Unload {} {
+    return
+}
