b495f90cc8f826c0cf6a19228613939099ad85a0
neoxed <neoxed@8dc05064-dfdd-11de-a668-6fd11605f5ce>
2005-12-18 21:11:47 +0000

    Read ioA's configuration file for paths to the search, requests, onel, nukes, and unnukes data files.

--- alcobot/trunk/modules/ioftpd/ioa/modIoA.tcl
+++ alcobot/trunk/modules/ioftpd/ioa/modIoA.tcl
@@ -22,6 +22,36 @@ namespace eval ::alcoholicz::IoA {
 # Module initialisation procedure, called when the module is loaded.
 #
 proc ::alcoholicz::IoA::Load {firstLoad} {
+    upvar ::alcoholicz::configHandle configHandle
+
+    # Open ioA's configuration file.
+    set ioaFile [ConfigGet $configHandle Module::IoA configFile]
+    if {![file isfile $ioaFile]} {
+        error "the file \"$ioaFile\" does not exist"
+    }
+    set ioaHandle [ConfigOpen $ioaFile]
+    ConfigRead $ioaHandle
+
+    foreach {varName section key} {
+        searchFile   Search  Search_Log_File
+        requestsFile Request Request_File
+        onelineFile  Oneline Oneline_File
+        nukesFile    Nuke    Nuke_Log_File
+        unnukesFile  Unnuke  UnNuke_Log_File
+    } {
+        set value [ConfigGet $ioaHandle $section $key]
+        variable $varName [string trim $value " \t\""]
+    }
+    ConfigClose $ioaHandle
+
+    # TODO: Created related commands.
+    #
+    # !search   ($searchFile)
+    # !requests ($requestsFile)
+    # !onel     ($onelineFile)
+    # !nukes    ($nukesFile)
+    # !unnukes  ($unnukesFile)
+
     return
 }
 
