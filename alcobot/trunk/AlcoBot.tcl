db079c4697b1ae5f7bab8030800f17744fae645b
neoxed <neoxed@8dc05064-dfdd-11de-a668-6fd11605f5ce>
2006-02-17 03:54:40 +0000

    Bump required AlcoExt version to 0.5.

--- alcobot/trunk/AlcoBot.tcl
+++ alcobot/trunk/AlcoBot.tcl
@@ -1088,7 +1088,7 @@ proc ::alcoholicz::InitLibraries {rootPath} {
 
     # The TLS extension is optional, but AlcoExt is required.
     catch {package require tls 1.5}
-    package require AlcoExt 0.4
+    package require AlcoExt 0.5
 }
 
 ####
