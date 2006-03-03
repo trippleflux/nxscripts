146028dbb71985aa4644db3008b7d7b1e27fa6f9
neoxed <neoxed@8dc05064-dfdd-11de-a668-6fd11605f5ce>
2006-03-03 22:27:27 +0000

    Moved variable procedures to the core.

--- alcobot/trunk/AlcoBot.tcl
+++ alcobot/trunk/AlcoBot.tcl
@@ -25,7 +25,9 @@ namespace eval ::alcoholicz {
         ModuleFind ModuleHash ModuleInfo ModuleLoad ModuleUnload ModuleRead \
         ScriptExecute ScriptRegister ScriptUnregister \
         GetFlagsFromSection GetSectionFromEvent GetSectionFromPath \
-        SendSection SendSectionTheme SendTarget SendTargetTheme
+        SendSection SendSectionTheme SendTarget SendTargetTheme \
+        VarReplace VarReplaceBase VarReplaceCommon
+
 }
 
 #
@@ -903,6 +905,230 @@ proc ::alcoholicz::SendTargetTheme {target type {valueList ""} {section ""}} {
     # since "[b]" is also the bold code).
     set text [VarReplaceCommon $theme($type) $section]
     SendTarget $target [VarReplace $text $variables($type) $valueList]
+}
+
+################################################################################
+# Variables                                                                    #
+################################################################################
+
+####
+# VarFormat
+#
+# Formats a given variable according to its substitution type.
+#
+proc ::alcoholicz::VarFormat {valueVar name type width precision} {
+    variable sizeDivisor
+    upvar $valueVar value
+
+    # Type of variable substitution. Error checking is not performed
+    # for all types because of the performance implications in doing so.
+    switch -- $type {
+        b {set value [FormatSize [expr {double($value) / double($sizeDivisor)}]]}
+        d {set value [FormatDuration $value]}
+        e {set value [subst -nocommands -novariables $value]}
+        k {set value [FormatSize $value]}
+        m {set value [FormatSize [expr {double($value) * double($sizeDivisor)}]]}
+        s {set value [FormatSpeed $value]}
+        p -
+        P -
+        t {return 0}
+        n {
+            # Integer or floating point.
+            if {[string is integer -strict $value]} {
+                set value [format "%${width}d" $value]
+                return 1
+            } elseif {[string is double -strict $value]} {
+                set value [format "%${width}.${precision}f" $value]
+                return 1
+            }
+            LogWarning VarFormat "Invalid numerical value \"$value\" for \"$name\"."
+        }
+        z {
+            variable replace
+            if {![string length $value] && [info exists replace($name)]} {
+                set value $replace($name)
+            }
+        }
+        default {LogWarning VarFormat "Unknown substitution type \"$type\" for \"$name\"."}
+    }
+
+    set value [format "%${width}.${precision}s" $value]
+    return 1
+}
+
+####
+# VarReplace
+#
+# Substitute a list of variables and values in a given string.
+#
+proc ::alcoholicz::VarReplace {input varList valueList} {
+    set inputLength [string length $input]
+    set output ""
+
+    set index 0
+    foreach varName $varList value $valueList {
+        if {[string match {*:[Ppt]} $varName]} {
+            set type [string index $varName end]
+            set varName [string range $varName 0 end-2]
+
+            if {$type eq "P" || $type eq "p"} {
+                lappend varList ${varName}Dir:z ${varName}Full:z ${varName}Name:z ${varName}Path:z
+                eval lappend valueList [PathParseSection $value [string equal $type "P"]]
+            } elseif {$type eq "t"} {
+                lappend varList ${varName}Date:z ${varName}Time:z
+                lappend valueList [FormatDate $value] [FormatTime $value]
+            }
+        } elseif {[llength $varName] > 1} {
+            variable theme
+            variable variables
+
+            set value [ArgsToList $value]
+            foreach {varName loopName} $varName {
+                set joinName "${loopName}_JOIN"
+                if {![info exists theme($loopName)] || ![info exists theme($joinName)]} {
+                    LogError VarReplace "Missing theme definition for \"$loopName\" or \"$joinName\"."
+                    continue
+                }
+
+                set data [list]
+                set varCount [llength $variables($loopName)]
+                set valueCount [llength $value]
+                for {set i 0} {$i < $valueCount} {incr i $varCount} {
+                    set values [lrange $value $i [expr {$i + $varCount - 1}]]
+                    lappend data [VarReplace $theme($loopName) $variables($loopName) $values]
+                }
+                lappend varList $varName
+                lappend valueList [join $data $theme($joinName)]
+            }
+        } else {
+            incr index; continue
+        }
+
+        # Remove the original variable and its value.
+        set varList [lreplace $varList $index $index]
+        set valueList [lreplace $valueList $index $index]
+    }
+
+    for {set inputIndex 0} {$inputIndex < $inputLength} {incr inputIndex} {
+        if {[string index $input $inputIndex] ne "%"} {
+            append output [string index $input $inputIndex]
+        } else {
+            set startIndex $inputIndex
+
+            # Find the width field (before dot).
+            set beforeIndex [incr inputIndex]
+            if {[string index $input $inputIndex] eq "-"} {
+                # Ignore the negative sign if a number does not follow, e.g. %-(variable).
+                if {[string is digit -strict [string index $input [incr inputIndex]]]} {
+                    incr inputIndex
+                } else {incr beforeIndex}
+            }
+            while {[string is digit -strict [string index $input $inputIndex]]} {incr inputIndex}
+            if {$beforeIndex != $inputIndex} {
+                set width [string range $input $beforeIndex [expr {$inputIndex - 1}]]
+            } else {
+                set width 0
+            }
+
+            # Find the precision field (after dot).
+            if {[string index $input $inputIndex] eq "."} {
+                set beforeIndex [incr inputIndex]
+                # Ignore the negative sign, ex: %.-(variable).
+                if {[string index $input $inputIndex] eq "-"} {incr beforeIndex; incr inputIndex}
+                while {[string is digit -strict [string index $input $inputIndex]]} {incr inputIndex}
+                if {$beforeIndex != $inputIndex} {
+                    set precision [string range $input $beforeIndex [expr {$inputIndex - 1}]]
+                } else {
+                    set precision 0
+                }
+            } else {
+                # Tcl's format function does not accept -1 for the precision field
+                # like printf() does, so a reasonably large number will suffice.
+                set precision 9999
+            }
+
+            # Find the variable name.
+            if {[string index $input $inputIndex] eq "("} {
+                set beforeIndex [incr inputIndex]
+                while {[string index $input $inputIndex] ne ")" && $inputIndex <= $inputLength} {incr inputIndex}
+                set varName [string range $input $beforeIndex [expr {$inputIndex - 1}]]
+            } else {
+                # Invalid variable format, an opening parenthesis is expected.
+                append output [string range $input $startIndex $inputIndex]
+                continue
+            }
+
+            # Variable names must be composed of alphanumeric
+            # and/or connector characters (a-z, A-Z, 0-9, and _).
+            if {[string is wordchar -strict $varName] && [set valueIndex [lsearch -glob $varList ${varName}:?]] != -1} {
+                set value [lindex $valueList $valueIndex]
+                set type [string index [lindex $varList $valueIndex] end]
+
+                if {[VarFormat value $varName $type $width $precision]} {
+                    append output $value; continue
+                }
+            }
+
+            # Unknown variable name, the string will still appended to
+            # notify the user of their mistake (most likely a typo).
+            append output [string range $input $startIndex $inputIndex]
+        }
+    }
+    return $output
+}
+
+####
+# VarReplaceBase
+#
+# Replaces static content and control codes (i.e. bold, colour, and underline).
+#
+proc ::alcoholicz::VarReplaceBase {text {doPrefix 1}} {
+    # Replace static variables.
+    set vars {siteName:z siteTag:z}
+    set values [list $::alcoholicz::siteName $::alcoholicz::siteTag]
+
+    if {$doPrefix} {
+        lappend vars prefix:z
+        lappend values $::alcoholicz::format(prefix)
+    }
+    set text [VarReplace $text $vars $values]
+
+    # Mapping for control code replacement.
+    set map [list {[b]} [b] {[c]} [c] {[o]} [o] {[r]} [r] {[u]} [u]]
+    return [subst -nocommands -novariables [string map $map $text]]
+}
+
+####
+# VarReplaceCommon
+#
+# Replace dynamic content, such as the current date, time, and section colours.
+#
+proc ::alcoholicz::VarReplaceCommon {text section} {
+    variable colours
+    variable defaultSection
+
+    set vars [list now:t]
+    set values [list [clock seconds]]
+    if {$section eq ""} {
+        # If no section is specified, use the default section.
+        set section $defaultSection
+    } else {
+        lappend vars "section:z"
+        lappend values $section
+
+        if {![info exists colours($section)]} {
+            # If there are no colours available for the current
+            # section, use the default section's colours.
+            set section $defaultSection
+        }
+    }
+
+    if {[info exists colours($section)]} {
+        set text [string map $colours($section) $text]
+    } else {
+        LogDebug VarReplaceCommon "No section colours defined for \"$section\"."
+    }
+    return [VarReplace $text $vars $values]
 }
 
 ################################################################################
