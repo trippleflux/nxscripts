d34f8e25fbf13eeb9e6709dd202ae90f4fc038b7
neoxed <neoxed@8dc05064-dfdd-11de-a668-6fd11605f5ce>
2006-05-02 07:09:12 +0000

    Move all libraries into a Tcl multi-package (ticket #17).

---
+++ alcolib/trunk/libs/config.tcl
@@ -0,0 +1,372 @@
+#
+# AlcoBot - Alcoholicz site bot.
+# Copyright (c) 2005-2006 Alcoholicz Scripting Team
+#
+# Module Name:
+#   Config Library
+#
+# Author:
+#   neoxed (neoxed@gmail.com) May 13, 2005
+#
+# Abstract:
+#   Implements a configuration file library to read from and write to
+#   INI-style configuration files.
+#
+# Procedures:
+#   Config::Open     <filePath> [-align <int>] [-comment <char>]
+#   Config::Change   <handle> <-align | -comment | -path> [value]
+#   Config::Close    <handle>
+#   Config::Free     <handle>
+#   Config::Read     <handle>
+#   Config::Write    <handle>
+#   Config::Keys     <handle> [pattern]
+#   Config::Sections <handle> <section> [pattern]
+#   Config::Exists   <handle> <section> [key]
+#   Config::Get      <handle> <section> <key>
+#   Config::GetEx    <handle> <section> [pattern]
+#   Config::GetMulti <handle> <section> <key> ...
+#   Config::Set      <handle> <section> [<key> <value>]
+#   Config::Unset    <handle> <section> [key]
+#
+
+package require alco::getopt 1.2
+package require alco::util 1.2
+
+namespace eval ::Config {
+    variable nextHandle
+    if {![info exists nextHandle]} {
+        set nextHandle 0
+    }
+}
+
+####
+# Config::Open
+#
+# Create a new configuration library handle. This handle is used by every config
+# procedure and must be closed by ::Config::Close. The "-align int" switch determines
+# whether values will be aligned when writing the configuration file. If -align
+# is greater than one, additional padding is used (n-1). The "-comment char"
+# switch sets the comment character, "#" by default.
+#
+proc ::Config::Open {filePath args} {
+    variable nextHandle
+
+    # Parse arguments.
+    array set option [list align 0 comment "#"]
+    GetOpt::Parse $args {{align integer} {comment arg}} option
+    if {[string length $option(comment)] != 1} {
+        error "invalid comment \"$option(comment)\": must be one character"
+    }
+    set handle "config$nextHandle"
+    upvar ::Config::$handle config
+
+    #
+    # Config Handle Contents
+    #
+    # ftp(align)   - Key alignment.
+    # ftp(comment) - Comment character.
+    # ftp(tree)    - Config data tree.
+    # ftp(path)    - Path to the config file.
+    #
+    array set config [list       \
+        align   $option(align)   \
+        comment $option(comment) \
+        tree    [Tree::Create]   \
+        path    $filePath        \
+    ]
+
+    incr nextHandle
+    return $handle
+}
+
+####
+# Config::Change
+#
+# Retrieve and modify options for a given configuration handle.
+#
+proc ::Config::Change {handle args} {
+    Acquire $handle config
+
+    # Retrieve an option.
+    if {[llength $args] == 1} {
+        set option [GetOpt::Element {-align -comment -path} [lindex $args 0]]
+        return $config([string range $option 1 end])
+    }
+
+    # Change options.
+    set prev [array get config]
+    if {[catch {
+        GetOpt::Parse $args {{align integer} {comment arg} {path arg}} config
+        if {[string length $config(comment)] != 1} {
+            error "invalid comment \"$config(comment)\": must be one character"
+        }
+    } message]} {
+        # Restore previous options.
+        array set config $prev
+        error $message
+    }
+}
+
+####
+# Config::Close
+#
+# Closes and invalidates the specified handle.
+#
+proc ::Config::Close {handle} {
+    Acquire $handle config
+    unset -nocomplain config
+    return
+}
+
+####
+# Config::Free
+#
+# Clears the internal tree structure, which contains all configuration data.
+#
+proc ::Config::Free {handle} {
+    Acquire $handle config
+    set config(tree) [Tree::Create]
+    return
+}
+
+####
+# Config::Read
+#
+# Reads the configuration file from disk. An error is raised if the file
+# cannot be opened for reading.
+#
+proc ::Config::Read {handle} {
+    Acquire $handle config
+    set fileHandle [open $config(path) r]
+
+    # Clear the tree structure before reading new data.
+    set config(tree) [Tree::Create]
+    set comments [list]
+    set section ""
+
+    foreach line [split [read $fileHandle] "\n"] {
+        set line [string trim $line]
+
+        if {[string index $line 0] eq $config(comment)} {
+            lappend comments $line
+        } elseif {[string match {\[*\]} $line]} {
+            set section [string range $line 1 end-1]
+            if {$section eq ""} {continue}
+
+            # The section key must only be created once,
+            # in case a section is defined multiple times.
+            if {![Tree::Exists $config(tree) $section]} {
+                Tree::Set config(tree) $section [Tree::Create data [Tree::Create] length 0]
+            }
+
+            if {[llength $comments]} {
+                set comments [concat [Tree::GetQuiet $config(tree) $section comments] $comments]
+                Tree::Set config(tree) $section comments $comments
+                set comments [list]
+            }
+        } elseif {[set index [string first "=" $line]] != -1} {
+            set key [string trimright [string range $line 0 [expr {$index - 1}]]]
+            if {$key eq "" || $section eq ""} {continue}
+
+            # The length of the longest key is used to align all
+            # values when commiting the configuration file to disk.
+            if {[string length $key] > [Tree::Get $config(tree) $section length]} {
+                Tree::Set config(tree) $section length [string length $key]
+            }
+
+            # Key comments must be placed before the line, not at the end.
+            set keyTree [Tree::Create value [string trimleft [string range $line [incr index] end]]]
+
+            if {[llength $comments]} {
+                Tree::Set keyTree comments $comments
+                set comments [list]
+            }
+            Tree::Set config(tree) $section data $key $keyTree
+        }
+    }
+
+    close $fileHandle
+    return
+}
+
+####
+# Config::Write
+#
+# Writes the configuration file to disk. An error is raised if the file
+# cannot be opened for writing.
+#
+proc ::Config::Write {handle} {
+    Acquire $handle config
+    set fileHandle [open $config(path) w]
+
+    Tree::For {section sectionTree} $config(tree) {
+        if {$config(align)} {
+            set length [Tree::Get $sectionTree length]
+            incr length [expr {$config(align) - 1}]
+        }
+
+        if {[Tree::Exists $sectionTree comments]} {
+            puts $fileHandle [join [Tree::Get $sectionTree comments] "\n"]
+        }
+        puts $fileHandle "\[$section\]"
+
+        Tree::For {key keyTree} [Tree::Get $sectionTree data] {
+            if {[Tree::Exists $keyTree comments]} {
+                puts $fileHandle [join [Tree::Get $keyTree comments] "\n"]
+            }
+
+            set value [Tree::Get $keyTree value]
+            if {$config(align)} {
+                # Align values to the longest key name.
+                puts $fileHandle [format "%-*s=%s" $length $key $value]
+            } else {
+                puts $fileHandle "$key=$value"
+            }
+        }
+        puts $fileHandle ""
+    }
+
+    close $fileHandle
+    return
+}
+
+####
+# Config::Keys
+#
+# Returns a list of all keys within a given section. If the "pattern" argument
+# is specified, only matching keys are returned.
+#
+proc ::Config::Keys {handle section {pattern "*"}} {
+    Acquire $handle config
+    return [Tree::Keys [Tree::GetQuiet $config(tree) $section data] $pattern]
+}
+
+####
+# Config::Sections
+#
+# Returns a list of all configuration sections. If the "pattern" argument is
+# specified, only matching sections are returned.
+#
+proc ::Config::Sections {handle {pattern "*"}} {
+    Acquire $handle config
+    return [Tree::Keys $config(tree) $pattern]
+}
+
+####
+# Config::Exists
+#
+# Test for the existence of a section or a key within a given section.
+#
+proc ::Config::Exists {handle section {key ""}} {
+    Acquire $handle config
+    if {[string length $key]} {
+        return [Tree::Exists $config(tree) $section data $key]
+    } else {
+        return [Tree::Exists $config(tree) $section]
+    }
+}
+
+####
+# Config::Get
+#
+# Returns the value of the named key from the specified section.
+#
+proc ::Config::Get {handle section key} {
+    Acquire $handle config
+    return [Tree::GetQuiet $config(tree) $section data $key value]
+}
+
+####
+# Config::GetEx
+#
+# Returns a list of key and value pairs from the specified section. If the
+# "pattern" argument is specified, only matching keys are returned.
+#
+proc ::Config::GetEx {handle section {pattern "*"}} {
+    Acquire $handle config
+    set result [list]
+    Tree::For {key keyTree} [Tree::GetQuiet $config(tree) $section data] {
+        if {[string match $pattern $key]} {
+            lappend result $key [Tree::Get $keyTree value]
+        }
+    }
+    return $result
+}
+
+####
+# Config::GetMulti
+#
+# Returns a list of key and value pairs from the specified section.
+#
+proc ::Config::GetMulti {handle section args} {
+    Acquire $handle config
+    set result [list]
+    set keyTree [Tree::GetQuiet $config(tree) $section data]
+    foreach key $args {
+        lappend result $key [Tree::GetQuiet $keyTree $key value]
+    }
+    return $result
+}
+
+####
+# Config::Set
+#
+# Sets the value of the key in the specified section. If the section does not
+# exist, a new one is created.
+#
+proc ::Config::Set {handle section args} {
+    Acquire $handle config
+    set argc [llength $args]
+    if {$argc != 0  && $argc != 2} {
+        error "wrong # args: must be \"Config::Set handle section ?key value?\""
+    }
+
+    # Initialise the section if it does not exist.
+    if {![Tree::Exists $config(tree) $section]} {
+        Tree::Set config(tree) $section [Tree::Create data [Tree::Create] length 0]
+    }
+
+    # Set the key's value and update the length if necessary.
+    if {$argc == 2} {
+        foreach {key value} $args {break}
+        if {[string length $key] > [Tree::Get $config(tree) $section length]} {
+            Tree::Set config(tree) $section length [string length $key]
+        }
+        Tree::Set config(tree) $section data $key value $value
+        return $value
+    }
+    return
+}
+
+####
+# Config::Unset
+#
+# Removes the key or the entire section and all its keys.
+#
+proc ::Config::Unset {handle section {key ""}} {
+    Acquire $handle config
+    if {$key eq ""} {
+        Tree::Unset config(tree) $section
+    } else {
+        Tree::Unset config(tree) $section data $key
+    }
+    return
+}
+
+################################################################################
+# Internal Procedures                                                          #
+################################################################################
+
+####
+# Acquire
+#
+# Validate and acquire a configuration handle.
+#
+proc ::Config::Acquire {handle handleVar} {
+    if {![regexp -- {config\d+} $handle] || ![array exists ::Config::$handle]} {
+        error "invalid config handle \"$handle\""
+    }
+    uplevel 1 [list upvar ::Config::$handle $handleVar]
+}
+
+package provide alco::config 1.2.0
---
+++ alcolib/trunk/libs/getopt.tcl
@@ -0,0 +1,187 @@
+#
+# AlcoBot - Alcoholicz site bot.
+# Copyright (c) 2005-2006 Alcoholicz Scripting Team
+#
+# Module Name:
+#   Option Parsing Library
+#
+# Author:
+#   neoxed (neoxed@gmail.com) Sep 8, 2005
+#
+# Abstract:
+#   Implements a command-line option parser.
+#
+# Error Handling:
+#   Errors thrown with the "GETOPT" error code indicate user errors, e.g. a
+#   user supplying an invalid or unknown command switch. Errors thrown without
+#   the "GETOPT" error code indicate a caller implementation problem.
+#
+# Procedures:
+#   GetOpt::Element <list> <element> [type]
+#   GetOpt::Index   <list> <element>
+#   GetOpt::Parse   <argList> <optList> <resultVar>
+#
+
+package require alco::util 1.2
+
+namespace eval ::GetOpt {
+    variable charClasses
+    if {![info exists charClasses]} {
+        # Create a list of known character classes.
+        catch {string is . .} charClasses
+        regexp -- {must be (.+)$} $charClasses dummy charClasses
+        regsub -all -- {, (or )?} $charClasses { } charClasses
+    }
+    namespace import -force ::Bot::GlobEscape
+    namespace import -force ::Bot::ListConvert
+}
+
+####
+# GetOpt::Element
+#
+# Simple wrapper around GetOpt::Index, an error is thrown if no match is found.
+#
+proc ::GetOpt::Element {list element {type "option"}} {
+    set index [Index $list $element]
+    if {$index == -1} {
+        throw GETOPT "invalid $type \"$element\": must be [ListConvert $list or]"
+    }
+    return [lindex $list $index]
+}
+
+####
+# GetOpt::Index
+#
+# Returns the index of an element in a list. A partial match is performed
+# if there is no exact match. If the element exists in the list, the index
+# is returned. If there is no unique match, -1 is returned.
+#
+proc ::GetOpt::Index {list element} {
+    # Check for an exact match.
+    set index [lsearch -exact $list $element]
+    if {$index == -1} {
+        # Check for a partial match (e.g. -foo will match -foobar).
+        set element [GlobEscape $element]; append element "*"
+        set index [lsearch -all -glob $list $element]
+
+        # If there is more than one partial match, a more specific
+        # list element must be provided for a unique match.
+        if {[llength $index] != 1} {
+            return -1
+        }
+    }
+    return $index
+}
+
+####
+# GetOpt::Parse
+#
+# Parses command line options from an argument list.
+#
+# Option List:
+#
+# option             - Option with no argument (e.g. "-option").
+#
+# option arg         - Option with any type of argument (e.g. "-option anything").
+#
+# option arg {a b c} - Option with predefined value (e.g. "-option b").
+#
+# option class       - Option with a specific type of value, the "class" can
+#                      be any character class supported by Tcl's "string is"
+#                      command. Such as alnum, alpha, ascii, control, boolean,
+#                      digit, double, false, graph, integer, lower, print, punct,
+#                      space, true, upper, wordchar, or xdigit
+#
+# Example:
+#
+# set argList "-limit 5 -match glob *some pattern*"
+# set optList {{limit integer} {match arg {exact glob regexp}}}
+# set pattern [GetOpt::Parse $argList $optList result]
+#
+# $result(limit) = 5
+# $result(match) = glob
+# $pattern       = *some pattern*"
+#
+proc ::GetOpt::Parse {argList optList resultVar} {
+    variable charClasses
+    upvar $resultVar result
+
+    if {[info exists result] && ![array exists result]} {
+        error "the variable \"$resultVar\" is not an array"
+    }
+
+    # Create a list of option names.
+    set optNames [list]
+    set types [lsort [concat $charClasses arg]]
+
+    foreach option $optList {
+        switch -- [llength $option] {
+            1 {}
+            2 {
+                if {[lsearch -exact $types [lindex $option 1]] == -1} {
+                    error "invalid option definition \"$option\": \
+                        bad type \"[lindex $option 1]\": must be [ListConvert $types or]"
+                }
+            }
+            3 {
+                if {[lindex $option 1] ne "arg"} {
+                    error "invalid option definition \"$option\": \
+                        value lists can only be used with the \"arg\" type"
+                }
+            }
+            default {
+                error "invalid option definition \"$option\": wrong number of arguments"
+            }
+        }
+        lappend optNames [lindex $option 0]
+    }
+
+    while {[set argCount [llength $argList]]} {
+        set arg [lindex $argList 0]
+
+        # The "--" argument indicates the end of options.
+        if {$arg eq "--"} {
+            set argList [lrange $argList 1 end]
+            break
+        }
+
+        if {[string index $arg 0] eq "-"} {
+            set index [Index $optNames [string range $arg 1 end]]
+            if {$index == -1} {
+                throw GETOPT "invalid option \"$arg\""
+            }
+
+            set optCount [llength [lindex $optList $index]]
+            foreach {optName optType optValues} [lindex $optList $index] {break}
+
+            if {$optCount > 1} {
+                if {$argCount < 2} {
+                    throw GETOPT "the option \"$arg\" requires a value"
+                }
+                set value [lindex $argList 1]
+
+                if {$optType eq "arg"} {
+                    if {$optCount > 2} {
+                        set value [Element $optValues $value "value"]
+                    }
+                } elseif {![string is $optType -strict $value]} {
+                    throw GETOPT "the option \"$arg\" requires a $optType type value"
+                }
+
+                set result($optName) $value
+                set argList [lrange $argList 2 end]
+            } else {
+                # Since this option does not require an argument, an empty
+                # string is used to notify the caller that it was specified.
+                set result($optName) {}
+                set argList [lrange $argList 1 end]
+            }
+        } else {
+            break
+        }
+    }
+
+    return $argList
+}
+
+package provide alco::getopt 1.2.0
