1ac176a2d79aaf5c3a715c6f3c4ee8b5a64f89f2
neoxed <neoxed@8dc05064-dfdd-11de-a668-6fd11605f5ce>
2006-05-14 07:48:42 +0000

    Retrieve the code blocks following a marked comment.

--- nxsdk/trunk/docs/gendocs.tcl
+++ nxsdk/trunk/docs/gendocs.tcl
@@ -27,33 +27,56 @@ set inputFiles {
 
 proc ReadFile {path} {
     set handle [open $path "r"]
-    set data [read -nonewline $handle]
+    set data [read $handle]
     close $handle
     return $data
 }
 
 proc ParseBlocks {text} {
-    set inBlock 0
+    set status 0
     set result [list]
 
     foreach line [split $text "\n"] {
+        set line [string trimright $line]
+
+        # status values:
+        # 0 - Ignore the line.
+        # 1 - In a marked block comment (/*++ ... --*/).
+        # 2 - After the end of a block comment.
+
         if {$line eq "/*++"} {
-            if {$inBlock} {
+            if {$status == 2} {
+                lappend result $desc $code
+                set code [list]
+                set desc [list]
+                set status 0
+            } elseif {$status == 1} {
                 puts "    - Found comment start but we are already in a comment block."
             } else {
-                set inBlock 1
-                set block [list]
+                set status 1
+                set code [list]
+                set desc [list]
             }
+
         } elseif {$line eq "--*/"} {
-            if {!$inBlock} {
+            if {$status != 1} {
                 puts "    - Found comment end but we are not in a comment block."
             } else {
-                lappend result $block
-                set inBlock 0
-                set block [list]
+                set status 2
             }
-        } elseif {$inBlock} {
-            lappend block $line
+
+        } elseif {$status == 2} {
+            if {$line eq ""} {
+                lappend result $desc $code
+                set code [list]
+                set desc [list]
+                set status 0
+            } else {
+                lappend code $line
+            }
+
+        } elseif {$status == 1} {
+            lappend desc $line
         }
     }
 
@@ -85,8 +108,11 @@ foreach path [lsort [array names data]] {
     puts $handle "# $path"
     puts $handle "################################################################################"
 
-    foreach block $data($path) {
-        foreach line $block {puts $handle $line}
+    foreach {desc code} $data($path) {
+        foreach line $desc {puts $handle $line}
+        puts $handle "########################################"
+        foreach line $code {puts $handle $line}
+
         puts $handle "############################################################"
     }
 }
