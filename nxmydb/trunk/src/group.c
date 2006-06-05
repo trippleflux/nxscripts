bcfb8de9bc9ea9dcf73be7909d89efa04643a3d7
neoxed <neoxed@8dc05064-dfdd-11de-a668-6fd11605f5ce>
2006-06-05 08:44:21 +0000

    Rewrote group functions to use the new backend system.

--- nxmydb/trunk/src/group.c
+++ nxmydb/trunk/src/group.c
@@ -17,7 +17,7 @@ Abstract:
 #include "mydb.h"
 
 //
-// Function and type declarations
+// Function declarations
 //
 
 static INT   MODULE_CALL GroupFinalize(void);
@@ -26,15 +26,9 @@ static INT   MODULE_CALL GroupRename(char *groupName, INT32 groupId, char *newNa
 static INT   MODULE_CALL GroupDelete(char *groupName, INT32 groupId);
 static INT   MODULE_CALL GroupLock(GROUPFILE *groupFile);
 static INT   MODULE_CALL GroupUnlock(GROUPFILE *groupFile);
-static INT   MODULE_CALL GroupRead(char *filePath, GROUPFILE *groupFile);
-static INT   MODULE_CALL GroupWrite(GROUPFILE *groupFile);
 static INT   MODULE_CALL GroupOpen(char *groupName, GROUPFILE *groupFile);
+static INT   MODULE_CALL GroupWrite(GROUPFILE *groupFile);
 static INT   MODULE_CALL GroupClose(GROUPFILE *groupFile);
-
-typedef struct {
-    HANDLE fileHandle;
-    volatile LONG locked;
-} GROUP_CONTEXT;
 
 //
 // Local variables
@@ -55,13 +49,13 @@ GroupModuleInit(
     module->tszModuleName = "NXMYDB";
     module->DeInitialize  = GroupFinalize;
     module->Create        = GroupCreate;
-    module->Delete        = GroupDelete;
     module->Rename        = GroupRename;
+    module->Delete        = GroupDelete;
     module->Lock          = GroupLock;
-    module->Write         = GroupWrite;
-    module->Open          = GroupOpen;
-    module->Close         = GroupClose;
     module->Unlock        = GroupUnlock;
+    module->Open          = GroupOpen;
+    module->Write         = GroupWrite;
+    module->Close         = GroupClose;
 
     // Initialize procedure table
     if (!InitProcTable(module->GetProc)) {
@@ -97,117 +91,60 @@ GroupCreate(
     char *groupName
     )
 {
-    char buffer[_MAX_NAME + 12];
-    char *sourcePath;
-    char *tempPath;
     DWORD error;
-    INT32 result;
+    INT32 groupId;
+    INT_CONTEXT *context;
     GROUPFILE groupFile;
 
     DebugPrint("GroupCreate: groupName=\"%s\"\n", groupName);
 
-    // Retrieve default location
-    sourcePath = Io_ConfigGetPath("Locations", "Group_Files", "Default.Group", NULL);
-    if (sourcePath == NULL) {
-        DebugPrint("GroupCreate: Unable to retrieve default file location.\n");
+    // Allocate internal context
+    context = Io_Allocate(sizeof(INT_CONTEXT));
+    if (context == NULL) {
+        DebugPrint("GroupCreate: Unable to allocate internal context.\n");
 
         SetLastError(ERROR_NOT_ENOUGH_MEMORY);
-        return -1;
-    }
-
-    // Retrieve temporary location
-    StringCchPrintfA(buffer, ARRAYSIZE(buffer), "%s.tmp", groupName);
-    tempPath = Io_ConfigGetPath("Locations", "Group_Files", buffer, NULL);
-    if (tempPath == NULL) {
-        DebugPrint("GroupCreate: Unable to retrieve temporary file location.\n");
-
-        // Free resources
-        Io_Free(sourcePath);
-
-        SetLastError(ERROR_NOT_ENOUGH_MEMORY);
-        return -1;
-    }
-
-    // Copy default file
-    if (!CopyFileA(sourcePath, tempPath, FALSE)) {
-        error = GetLastError();
-        DebugPrint("GroupCreate: Unable to copy default file (error %lu).\n", error);
-
-        // Free resources
-        Io_Free(sourcePath);
-        Io_Free(tempPath);
-
-        // Restore system error code
-        SetLastError(error);
         return -1;
     }
 
     // Initialize GROUPFILE structure
     ZeroMemory(&groupFile, sizeof(GROUPFILE));
-
-    if (GroupRead(tempPath, &groupFile) != GM_SUCCESS) {
-        error = GetLastError();
-
-        // Free resources
-        DeleteFileA(tempPath);
-        Io_Free(sourcePath);
-        Io_Free(tempPath);
-
-        // Restore system error code
-        SetLastError(error);
-        return -1;
-    }
+    groupFile.lpInternal = context;
 
     // Register group
-    result = groupModule->Register(groupModule, groupName, &groupFile);
-    if (result == -1) {
-        error = GetLastError();
-
-        GroupClose(&groupFile);
-        DeleteFileA(tempPath);
-    } else {
-        char *offset;
-        size_t sourceLength = strlen(sourcePath);
-
-        // Terminate string after the last path separator
-        offset = strrchr(sourcePath, '\\');
-        if (offset != NULL) {
-            offset[1] = '\0';
-        } else {
-            offset = strrchr(sourcePath, '/');
-            if (offset != NULL) {
-                offset[1] = '\0';
-            }
-        }
-
-        // Append the group's ID
-        StringCchPrintfA(buffer, ARRAYSIZE(buffer), "%i", result);
-        StringCchCatA(sourcePath, sourceLength, buffer);
-
-        // Rename temporary file
-        if (!MoveFileExA(tempPath, sourcePath, MOVEFILE_REPLACE_EXISTING)) {
-            error = GetLastError();
-            DebugPrint("GroupCreate: Unable to rename temporary file (error %lu).\n", error);
-
-            // Unregister group and delete the data file
-            if (!groupModule->Unregister(groupModule, groupName)) {
-                GroupClose(&groupFile);
-            }
-            DeleteFileA(tempPath);
-
-            result = -1;
-        } else {
-            error = ERROR_SUCCESS;
-        }
+    groupId = groupModule->Register(groupModule, groupName, &groupFile);
+    if (groupId == -1) {
+        DebugPrint("GroupCreate: Unable to register group (error %lu).\n", GetLastError());
+        goto error;
     }
 
-    // Free resources
-    Io_Free(sourcePath);
-    Io_Free(tempPath);
+    // Create group file and read "Default.Group"
+    if (!FileGroupCreate(groupName, groupId, &groupFile)) {
+        DebugPrint("GroupCreate: Unable to create group file (error %lu).\n", GetLastError());
+        goto error;
+    }
+
+    // Create database record
+    if (!DbGroupCreate(groupName, groupId, &groupFile)) {
+        DebugPrint("GroupCreate: Unable to create database record (error %lu).\n", GetLastError());
+        goto error;
+    }
+
+    return groupId;
+
+error:
+    // Preserve system error code
+    error = GetLastError();
+
+    if (groupId == -1) {
+        Io_Free(context);
+    } else if (GroupDelete(groupName, groupId) != GM_SUCCESS) {
+        DebugPrint("GroupCreate: Unable to delete group (error %lu).\n", GetLastError());
+    }
 
     // Restore system error code
     SetLastError(error);
-    return result;
+    return -1;
 }
 
 static
@@ -221,9 +158,14 @@ GroupRename(
 {
     DebugPrint("GroupRename: groupName=\"%s\" groupId=%i newName=\"%s\"\n", groupName, groupId, newName);
 
-    // Register the group under the new name
-    if (groupModule->RegisterAs(groupModule, groupName, newName)) {
-        DebugPrint("GroupRename: Unable to rename group, already exists?\n");
+    // Rename database record
+    if (!DbGroupRename(groupName, groupId, newName)) {
+        DebugPrint("GroupRename: Unable to rename database record (error %lu).\n", GetLastError());
+    }
+
+    // Register group under the new name
+    if (groupModule->RegisterAs(groupModule, groupName, newName) != GM_SUCCESS) {
+        DebugPrint("GroupRename: Unable to re-register group (error %lu).\n", GetLastError());
         return GM_ERROR;
     }
 
@@ -238,30 +180,21 @@ GroupDelete(
     INT32 groupId
     )
 {
-    char buffer[16];
-    char *filePath;
-
     DebugPrint("GroupDelete: groupName=\"%s\" groupId=%i\n", groupName, groupId);
 
-    // Format group ID
-    StringCchPrintfA(buffer, ARRAYSIZE(buffer), "%i", groupId);
-
-    // Retrieve file location
-    filePath = Io_ConfigGetPath("Locations", "Group_Files", buffer, NULL);
-    if (filePath == NULL) {
-        DebugPrint("GroupDelete: Unable to retrieve file location.\n");
-
-        SetLastError(ERROR_NOT_ENOUGH_MEMORY);
-        return GM_ERROR;
+    // Delete group file
+    if (!FileGroupDelete(groupName, groupId)) {
+        DebugPrint("GroupDelete: Unable to delete group file (error %lu).\n", GetLastError());
     }
 
-    // Delete data file and free resources
-    DeleteFileA(filePath);
-    Io_Free(filePath);
+    // Delete database record
+    if (!DbGroupDelete(groupName, groupId)) {
+        DebugPrint("GroupDelete: Unable to delete database record (error %lu).\n", GetLastError());
+    }
 
     // Unregister group
-    if (groupModule->Unregister(groupModule, groupName)) {
-        DebugPrint("GroupDelete: Unable to unregister group.\n");
+    if (groupModule->Unregister(groupModule, groupName) != GM_SUCCESS) {
+        DebugPrint("GroupDelete: Unable to unregister group (error %lu).\n", GetLastError());
         return GM_ERROR;
     }
 
@@ -275,12 +208,11 @@ GroupLock(
     GROUPFILE *groupFile
     )
 {
-    GROUP_CONTEXT *context;
     DebugPrint("GroupLock: groupFile=%p\n", groupFile);
 
-    context = (GROUP_CONTEXT *)groupFile->lpInternal;
-    if (InterlockedCompareExchange(&context->locked, 1, 0) == 1) {
-        DebugPrint("GroupLock: Unable to aquire lock.\n");
+    // Lock group exclusively
+    if (!DbGroupLock(groupFile)) {
+        DebugPrint("GroupLock: Unable to lock database record (error %lu).\n", GetLastError());
         return GM_ERROR;
     }
 
@@ -294,156 +226,14 @@ GroupUnlock(
     GROUPFILE *groupFile
     )
 {
-    GROUP_CONTEXT *context;
     DebugPrint("GroupUnlock: groupFile=%p\n", groupFile);
 
-    // Clear locked flag.
-    context = (GROUP_CONTEXT *)groupFile->lpInternal;
-    context->locked = 0;
-
-    return GM_SUCCESS;
-}
-
-static
-INT
-MODULE_CALL
-GroupRead(
-    char *filePath,
-    GROUPFILE *groupFile
-    )
-{
-    char *buffer = NULL;
-    DWORD bytesRead;
-    DWORD error;
-    DWORD fileSize;
-    INT result = GM_FATAL;
-    GROUP_CONTEXT *context;
-
-    DebugPrint("GroupRead: filePath=\"%s\" groupFile=%p\n", filePath, groupFile);
-
-    // Allocate group context
-    context = (GROUP_CONTEXT *)Io_Allocate(sizeof(GROUP_CONTEXT));
-    if (context == NULL) {
-        DebugPrint("GroupRead: Unable to allocate group context.\n");
-
-        SetLastError(ERROR_NOT_ENOUGH_MEMORY);
-        return GM_FATAL;
-    }
-    context->locked = 0;
-
-    // Open the group's data file
-    context->fileHandle = CreateFileA(filePath,
-        GENERIC_READ|GENERIC_WRITE,
-        FILE_SHARE_READ|FILE_SHARE_WRITE|FILE_SHARE_DELETE,
-        NULL, OPEN_EXISTING, 0, NULL);
-
-    if (context->fileHandle == INVALID_HANDLE_VALUE) {
-        result = (GetLastError() == ERROR_FILE_NOT_FOUND) ? GM_DELETED : GM_FATAL;
-        DebugPrint("GroupRead: Unable to open file (error %lu).\n", GetLastError());
-        goto end;
-    }
-
-    // Retrieve file size
-    fileSize = GetFileSize(context->fileHandle, NULL);
-    if (fileSize == INVALID_FILE_SIZE || fileSize < 5) {
-        DebugPrint("GroupRead: Unable to retrieve file size, or file size is under 5 bytes.\n");
-        goto end;
-    }
-
-    // Allocate read buffer
-    buffer = (char *)Io_Allocate(fileSize + 1);
-    if (buffer == NULL) {
-        DebugPrint("GroupRead: Unable to allocate read buffer.\n");
-        goto end;
-    }
-
-    // Read data file to buffer
-    if (!ReadFile(context->fileHandle, buffer, fileSize, &bytesRead, NULL) || bytesRead < 5) {
-        DebugPrint("GroupRead: Unable to read file, or the amount read is under 5 bytes.\n");
-        goto end;
-    }
-
-    // Pad buffer with a new-line
-    buffer[bytesRead] = '\n';
-    bytesRead++;
-
-    // Parse buffer, initializing the GROUPFILE structure
-    Io_Ascii2GroupFile(buffer, bytesRead, groupFile);
-    groupFile->lpInternal = context;
-    result                = GM_SUCCESS;
-
-end:
-    // Free objects and resources
-    if (result != GM_SUCCESS) {
-        error = GetLastError();
-        if (context->fileHandle != INVALID_HANDLE_VALUE) {
-            CloseHandle(context->fileHandle);
-        }
-        Io_Free(buffer);
-        Io_Free(context);
-        groupFile->lpInternal = NULL;
-
-        // Restore system error code
-        SetLastError(error);
-    } else {
-        Io_Free(buffer);
-    }
-
-    return result;
-}
-
-static
-INT
-MODULE_CALL
-GroupWrite(
-    GROUPFILE *groupFile
-    )
-{
-    BUFFER buffer;
-    DWORD bytesWritten;
-    DWORD error;
-    GROUP_CONTEXT *context;
-
-    DebugPrint("GroupWrite: groupFile=%p\n", groupFile);
-
-    // Retrieve group context
-    context = (GROUP_CONTEXT *)groupFile->lpInternal;
-
-    // Allocate write buffer
-    ZeroMemory(&buffer, sizeof(BUFFER));
-    buffer.dwType = TYPE_CHAR;
-    buffer.size   = 4096;
-    buffer.buf    = (char *)Io_Allocate(buffer.size);
-
-    if (buffer.buf == NULL) {
-        DebugPrint("GroupWrite: Unable to allocate write buffer.\n");
-
-        SetLastError(ERROR_NOT_ENOUGH_MEMORY);
+    // Unlock group
+    if (!DbGroupUnlock(groupFile)) {
+        DebugPrint("GroupUnlock: Unable to unlock database record (error %lu).\n", GetLastError());
         return GM_ERROR;
     }
 
-    // Dump group data to buffer
-    Io_GroupFile2Ascii(&buffer, groupFile);
-
-    // Write buffer to file
-    SetFilePointer(context->fileHandle, 0, 0, FILE_BEGIN);
-    if (!WriteFile(context->fileHandle, buffer.buf, buffer.len, &bytesWritten, NULL)) {
-        error = GetLastError();
-        DebugPrint("GroupWrite: Unable to write file (error %lu).\n", error);
-
-        // Free resources
-        Io_Free(buffer.buf);
-
-        // Restore system error code
-        SetLastError(error);
-        return GM_ERROR;
-    }
-
-    // Truncate file at its current position and flush changes to disk
-    SetEndOfFile(context->fileHandle);
-    FlushFileBuffers(context->fileHandle);
-
-    Io_Free(buffer.buf);
     return GM_SUCCESS;
 }
 
@@ -455,27 +245,53 @@ GroupOpen(
     GROUPFILE *groupFile
     )
 {
-    char buffer[16];
-    char *filePath;
-    INT result;
-
     DebugPrint("GroupOpen: groupName=\"%s\" groupFile=%p\n", groupName, groupFile);
 
-    // Format group ID
-    StringCchPrintfA(buffer, ARRAYSIZE(buffer), "%i", groupFile->Gid);
+    // Allocate internal context
+    groupFile->lpInternal = Io_Allocate(sizeof(INT_CONTEXT));
+    if (groupFile->lpInternal == NULL) {
+        DebugPrint("GroupOpen: Unable to allocate internal context.\n");
 
-    // Retrieve file location
-    filePath = Io_ConfigGetPath("Locations", "Group_Files", buffer, NULL);
-    if (filePath == NULL) {
-        DebugPrint("GroupOpen: Unable to retrieve file location.\n");
+        SetLastError(ERROR_NOT_ENOUGH_MEMORY);
         return GM_FATAL;
     }
 
-    // Read the group's data file
-    result = GroupRead(filePath, groupFile);
-    Io_Free(filePath);
+    // Open group file
+    if (!FileGroupOpen(groupName, groupFile)) {
+        DebugPrint("GroupOpen: Unable to open group file (error %lu).\n", GetLastError());
+        return (GetLastError() == ERROR_FILE_NOT_FOUND) ? GM_DELETED : GM_FATAL;
+    }
 
-    return result;
+    // Read database record
+    if (!DbGroupOpen(groupName, groupFile)) {
+        DebugPrint("GroupOpen: Unable to open database record (error %lu).\n", GetLastError());
+        return GM_FATAL;
+    }
+
+    return GM_SUCCESS;
+}
+
+static
+INT
+MODULE_CALL
+GroupWrite(
+    GROUPFILE *groupFile
+    )
+{
+    DebugPrint("GroupWrite: groupFile=%p\n", groupFile);
+
+    // Update group file
+    if (!FileGroupWrite(groupFile)) {
+        DebugPrint("GroupWrite: Unable to write group file (error %lu).\n", GetLastError());
+    }
+
+    // Update database record
+    if (!DbGroupWrite(groupFile)) {
+        DebugPrint("GroupWrite: Unable to write database record (error %lu).\n", GetLastError());
+        return GM_ERROR;
+    }
+
+    return GM_SUCCESS;
 }
 
 static
@@ -485,19 +301,27 @@ GroupClose(
     GROUPFILE *groupFile
     )
 {
-    GROUP_CONTEXT *context;
+    INT_CONTEXT *context = groupFile->lpInternal;
 
     DebugPrint("GroupClose: groupFile=%p\n", groupFile);
 
-    // Retrieve group context
-    context = (GROUP_CONTEXT *)groupFile->lpInternal;
+    // Verify internal context
     if (context == NULL) {
-        DebugPrint("GroupClose: Group context already freed.\n");
+        DebugPrint("GroupClose: Internal context already freed.\n");
         return GM_ERROR;
     }
 
-    // Free objects and resources
-    CloseHandle(context->fileHandle);
+    // Close group file
+    if (!FileGroupClose(context)) {
+        DebugPrint("GroupClose: Unable to close group file (error %lu).\n", GetLastError());
+    }
+
+    // Free database resources
+    if (!DbGroupClose(context)) {
+        DebugPrint("GroupClose: Unable to close database record(error %lu).\n", GetLastError());
+    }
+
+    // Free internal resources
     Io_Free(context);
     groupFile->lpInternal = NULL;
 
---
+++ nxmydb/trunk/src/groupfile.c
@@ -0,0 +1,336 @@
+/*
+
+nxMyDB - MySQL Database for ioFTPD
+Copyright (c) 2006 neoxed
+
+Module Name:
+    Group File Backend
+
+Author:
+    neoxed (neoxed@gmail.com) Jun 5, 2006
+
+Abstract:
+    Group file storage backend.
+
+*/
+
+#include "mydb.h"
+
+static
+BOOL
+FileGroupRead(
+    char *filePath,
+    GROUPFILE *groupFile
+    )
+{
+    char *buffer = NULL;
+    DWORD bytesRead;
+    DWORD error;
+    DWORD fileSize;
+    INT_CONTEXT *context = groupFile->lpInternal;
+
+    DebugPrint("FileGroupRead: filePath=\"%s\" groupFile=%p\n", filePath, groupFile);
+
+    // Open the group file
+    context->fileHandle = CreateFileA(filePath,
+        GENERIC_READ|GENERIC_WRITE,
+        FILE_SHARE_READ|FILE_SHARE_WRITE|FILE_SHARE_DELETE,
+        NULL, OPEN_EXISTING, 0, NULL);
+
+    if (context->fileHandle == INVALID_HANDLE_VALUE) {
+        DebugPrint("FileGroupRead: Unable to open file (error %lu).\n", GetLastError());
+        goto error;
+    }
+
+    // Retrieve file size
+    fileSize = GetFileSize(context->fileHandle, NULL);
+    if (fileSize == INVALID_FILE_SIZE || fileSize < 5) {
+        DebugPrint("FileGroupRead: Unable to retrieve file size, or file size is under 5 bytes.\n");
+        goto error;
+    }
+
+    // Allocate read buffer
+    buffer = Io_Allocate(fileSize + 1);
+    if (buffer == NULL) {
+        DebugPrint("FileGroupRead: Unable to allocate read buffer.\n");
+        goto error;
+    }
+
+    // Read group file to buffer
+    if (!ReadFile(context->fileHandle, buffer, fileSize, &bytesRead, NULL) || bytesRead < 5) {
+        DebugPrint("FileGroupRead: Unable to read file, or the amount read is under 5 bytes.\n");
+        goto error;
+    }
+
+    // Pad buffer with a new-line
+    buffer[bytesRead] = '\n';
+    bytesRead++;
+
+    // Parse buffer, also initializing the GROUPFILE structure
+    Io_Ascii2GroupFile(buffer, bytesRead, groupFile);
+
+    // Free resources
+    Io_Free(buffer);
+
+    return TRUE;
+
+error:
+    // Free objects and resources
+    error = GetLastError();
+    if (context->fileHandle != INVALID_HANDLE_VALUE) {
+        CloseHandle(context->fileHandle);
+    }
+    Io_Free(buffer);
+    Io_Free(context);
+    groupFile->lpInternal = NULL;
+
+    // Restore system error code
+    SetLastError(error);
+    return FALSE;
+}
+
+BOOL
+FileGroupCreate(
+    char *groupName,
+    INT32 groupId,
+    GROUPFILE *groupFile
+    )
+{
+    char *defaultPath;
+    char *targetPath;
+    char buffer[12];
+    DWORD error;
+
+    DebugPrint("FileGroupCreate: groupName=\"%s\"\n", groupName);
+
+    // Retrieve default group location
+    defaultPath = Io_ConfigGetPath("Locations", "Group_Files", "Default.Group", NULL);
+    if (defaultPath == NULL) {
+        DebugPrint("FileGroupCreate: Unable to retrieve default file location.\n");
+
+        SetLastError(ERROR_NOT_ENOUGH_MEMORY);
+        return FALSE;
+    }
+
+    // Retrieve group location
+    StringCchPrintfA(buffer, ARRAYSIZE(buffer), "%i", groupId);
+    targetPath = Io_ConfigGetPath("Locations", "Group_Files", buffer, NULL);
+    if (targetPath == NULL) {
+        DebugPrint("FileGroupCreate: Unable to retrieve file location.\n");
+
+        // Free resources
+        Io_Free(defaultPath);
+
+        SetLastError(ERROR_NOT_ENOUGH_MEMORY);
+        return FALSE;
+    }
+
+    // Copy default file to target file
+    if (!CopyFileA(defaultPath, targetPath, FALSE)) {
+        error = GetLastError();
+        DebugPrint("FileGroupCreate: Unable to copy default file (error %lu).\n", error);
+
+        // Free resources
+        Io_Free(defaultPath);
+        Io_Free(targetPath);
+
+        // Restore system error code
+        SetLastError(error);
+        return FALSE;
+    }
+
+    // Read group file (copy of "Default.Group")
+    if (!FileGroupRead(targetPath, groupFile)) {
+        error = GetLastError();
+        DebugPrint("FileGroupCreate: Unable read target file (error %lu).\n", error);
+
+        // Free resources
+        Io_Free(defaultPath);
+        Io_Free(targetPath);
+
+        // Restore system error code
+        SetLastError(error);
+        return FALSE;
+    }
+
+    // Free resources
+    Io_Free(defaultPath);
+    Io_Free(targetPath);
+
+    return TRUE;
+}
+
+BOOL
+FileGroupRename(
+    char *groupName,
+    INT32 groupId,
+    char *newName
+    )
+{
+    DebugPrint("FileGroupRename: groupName=\"%s\" groupId=%i newName=\"%s\"\n", groupName, groupId, newName);
+    // Not used
+    return TRUE;
+}
+
+BOOL
+FileGroupDelete(
+    char *groupName,
+    INT32 groupId
+    )
+{
+    char buffer[12];
+    char *filePath;
+
+    DebugPrint("FileGroupDelete: groupName=\"%s\" groupId=%i\n", groupName, groupId);
+
+    // Retrieve group file location
+    StringCchPrintfA(buffer, ARRAYSIZE(buffer), "%i", groupId);
+    filePath = Io_ConfigGetPath("Locations", "Group_Files", buffer, NULL);
+    if (filePath == NULL) {
+        DebugPrint("FileGroupDelete: Unable to retrieve file location.\n");
+
+        SetLastError(ERROR_NOT_ENOUGH_MEMORY);
+        return FALSE;
+    }
+
+    // Delete group file and free resources
+    DeleteFileA(filePath);
+    Io_Free(filePath);
+
+    return TRUE;
+}
+
+BOOL
+FileGroupLock(
+    GROUPFILE *groupFile
+    )
+{
+    DebugPrint("FileGroupLock: groupFile=%p\n", groupFile);
+    // Not used
+    return TRUE;
+}
+
+BOOL
+FileGroupUnlock(
+    GROUPFILE *groupFile
+    )
+{
+    DebugPrint("FileGroupUnlock: groupFile=%p\n", groupFile);
+    // Not used
+    return TRUE;
+}
+
+BOOL
+FileGroupOpen(
+    char *groupName,
+    GROUPFILE *groupFile
+    )
+{
+    char buffer[12];
+    char *filePath;
+    DWORD error;
+    INT_CONTEXT *context = groupFile->lpInternal;
+
+    DebugPrint("FileGroupOpen: groupName=\"%s\" groupFile=%p\n", groupName, groupFile);
+
+    // Retrieve group file location
+    StringCchPrintfA(buffer, ARRAYSIZE(buffer), "%i", groupFile->Gid);
+    filePath = Io_ConfigGetPath("Locations", "Group_Files", buffer, NULL);
+    if (filePath == NULL) {
+        DebugPrint("FileGroupOpen: Unable to retrieve file location.\n");
+
+        SetLastError(ERROR_NOT_ENOUGH_MEMORY);
+        return FALSE;
+    }
+
+    // Open group file
+    context->fileHandle = CreateFileA(filePath,
+        GENERIC_READ|GENERIC_WRITE,
+        FILE_SHARE_READ|FILE_SHARE_WRITE|FILE_SHARE_DELETE,
+        NULL, OPEN_EXISTING, 0, NULL);
+
+    if (context->fileHandle == INVALID_HANDLE_VALUE) {
+        error = GetLastError();
+        DebugPrint("FileGroupOpen: Unable to open file (error %lu).\n", error);
+
+        // Free resources
+        Io_Free(filePath);
+
+        // Restore system error code
+        SetLastError(error);
+        return FALSE;
+    }
+
+    // Free resources
+    Io_Free(filePath);
+
+    return TRUE;
+}
+
+BOOL
+FileGroupWrite(
+    GROUPFILE *groupFile
+    )
+{
+    BUFFER buffer;
+    DWORD bytesWritten;
+    DWORD error;
+    INT_CONTEXT *context = groupFile->lpInternal;
+
+    DebugPrint("FileGroupWrite: groupFile=%p\n", groupFile);
+
+    // Allocate write buffer
+    ZeroMemory(&buffer, sizeof(BUFFER));
+    buffer.dwType = TYPE_CHAR;
+    buffer.size   = 4096;
+    buffer.buf    = Io_Allocate(buffer.size);
+
+    if (buffer.buf == NULL) {
+        DebugPrint("FileGroupWrite: Unable to allocate write buffer.\n");
+
+        SetLastError(ERROR_NOT_ENOUGH_MEMORY);
+        return FALSE;
+    }
+
+    // Dump group data to buffer
+    Io_GroupFile2Ascii(&buffer, groupFile);
+
+    // Write buffer to file
+    SetFilePointer(context->fileHandle, 0, 0, FILE_BEGIN);
+    if (!WriteFile(context->fileHandle, buffer.buf, buffer.len, &bytesWritten, NULL)) {
+        error = GetLastError();
+        DebugPrint("FileGroupWrite: Unable to write file (error %lu).\n", error);
+
+        // Free resources
+        Io_Free(buffer.buf);
+
+        // Restore system error code
+        SetLastError(error);
+        return FALSE;
+    }
+
+    // Truncate file at its current position and flush changes to disk
+    SetEndOfFile(context->fileHandle);
+    FlushFileBuffers(context->fileHandle);
+
+    // Free resources
+    Io_Free(buffer.buf);
+
+    return TRUE;
+}
+
+BOOL
+FileGroupClose(
+    INT_CONTEXT *context
+    )
+{
+    DebugPrint("FileGroupClose: context=%p\n", context);
+
+    // Close group file handle
+    if (context->fileHandle != INVALID_HANDLE_VALUE) {
+        CloseHandle(context->fileHandle);
+        context->fileHandle = INVALID_HANDLE_VALUE;
+    }
+
+    return TRUE;
+}
