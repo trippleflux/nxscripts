5cf7d5b3788231bd20b15c0e1f40b6db3daa7507
neoxed <neoxed@8dc05064-dfdd-11de-a668-6fd11605f5ce>
2006-06-14 03:51:03 +0000

    Read and validate configuration options.

--- nxmydb/trunk/src/dbconn.c
+++ nxmydb/trunk/src/dbconn.c
@@ -16,6 +16,15 @@ Abstract:
 
 #include "mydb.h"
 
+// MySQL server information
+static char *serverHost = NULL;
+static char *serverUser = NULL;
+static char *serverPass = NULL;
+static char *serverDb   = NULL;
+static int   serverPort = 3306;
+static BOOL  useCompression = FALSE;
+static BOOL  useEncryption  = FALSE;
+
 // Reference count initialization calls
 static int refCount = 0;
 
@@ -45,6 +54,10 @@ DbInit(
     Io_GetProc *getProc
     )
 {
+    int poolMin;
+    int poolMax;
+    int poolKeepAlive;
+    int poolTimeout;
     DebugPrint("DbInit", "getProc=%p refCount=%i\n", getProc, refCount);
 
     // Only initialize the module once
@@ -58,6 +71,52 @@ DbInit(
         DebugPrint("DbInit", "Unable to initialize procedure table.\n");
         return FALSE;
     }
+
+    // Read pool options
+    poolMin = 2;
+    if (Io_ConfigGetInt("nxMyDB", "Pool_Minimum", &poolMin) && poolMin <= 0) {
+        Io_Putlog(LOG_ERROR, "nxMyDB: Option 'Pool_Minimum' must be greater than zero.\r\n");
+        return FALSE;
+    }
+
+    poolMax = poolMin * 2;
+    if (Io_ConfigGetInt("nxMyDB", "Pool_Maximum", &poolMax) && poolMax <= poolMin) {
+        Io_Putlog(LOG_ERROR, "nxMyDB: Option 'Pool_Maximum' must be greater than 'Pool_Minimum'.\r\n");
+        return FALSE;
+    }
+
+    poolKeepAlive = 3600;
+    if (Io_ConfigGetInt("nxMyDB", "Pool_Keep_Alive", &poolKeepAlive) && poolKeepAlive <= 0) {
+        Io_Putlog(LOG_ERROR, "nxMyDB: Option 'Pool_Keep_Alive' must be greater than zero.\r\n");
+        return FALSE;
+    }
+
+    poolTimeout = 5;
+    if (Io_ConfigGetInt("nxMyDB", "Pool_Timeout", &poolTimeout) && poolTimeout <= 0) {
+        Io_Putlog(LOG_ERROR, "nxMyDB: Option 'Pool_Timeout' must be greater than zero.\r\n");
+        return FALSE;
+    }
+
+    // Read server options
+    serverHost = Io_ConfigGet("nxMyDB", "Host", NULL, NULL);
+    serverUser = Io_ConfigGet("nxMyDB", "User", NULL, NULL);
+    serverPass = Io_ConfigGet("nxMyDB", "Password", NULL, NULL);
+    serverDb   = Io_ConfigGet("nxMyDB", "Database", NULL, NULL);
+    Io_ConfigGetInt("nxMyDB", "Port", &serverPort);
+    Io_ConfigGetBool("nxMyDB", "Compression", &useCompression);
+    Io_ConfigGetBool("nxMyDB", "Encryption", &useEncryption);
+
+    DebugPrint("DbInit", "Host           =%s\n", serverHost);
+    DebugPrint("DbInit", "Port           =%i\n", serverPort);
+    DebugPrint("DbInit", "User           =%s\n", serverUser);
+    DebugPrint("DbInit", "Password       =%s\n", serverPass);
+    DebugPrint("DbInit", "Database       =%s\n", serverDb);
+    DebugPrint("DbInit", "Compression    =%s\n", useCompression);
+    DebugPrint("DbInit", "Encryption     =%s\n", useEncryption);
+    DebugPrint("DbInit", "Pool_Minimum   =%i\n", poolMin);
+    DebugPrint("DbInit", "Pool_Maximum   =%i\n", poolMax);
+    DebugPrint("DbInit", "Pool_Keep_Alive=%i\n", poolKeepAlive);
+    DebugPrint("DbInit", "Pool_Timeout   =%i\n", poolTimeout);
 
     Io_Putlog(LOG_ERROR, "nxMyDB: v%s loaded.\r\n", STRINGIFY(VERSION));
     return TRUE;
@@ -90,6 +149,21 @@ DbFinalize(
     if (--refCount == 0) {
         Io_Putlog(LOG_ERROR, "nxMyDB: v%s unloaded.\r\n", STRINGIFY(VERSION));
 
+        // Free options
+        if (serverHost != NULL) {
+            Io_Free(serverHost);
+        }
+        if (serverUser != NULL) {
+            Io_Free(serverUser);
+        }
+        if (serverPass != NULL) {
+            Io_Free(serverPass);
+        }
+        if (serverDb != NULL) {
+            Io_Free(serverDb);
+        }
+
+        // Clear procedure table
         ProcTableFinalize();
     }
 }
