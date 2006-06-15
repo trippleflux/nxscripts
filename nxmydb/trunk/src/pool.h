63b68c19fcc9377bd7f4c71ac4a9d9a6edd722e7
neoxed <neoxed@8dc05064-dfdd-11de-a668-6fd11605f5ce>
2006-06-15 00:33:18 +0000

    Rename the queue structures and improve comments.

--- nxmydb/trunk/src/pool.h
+++ nxmydb/trunk/src/pool.h
@@ -36,7 +36,7 @@ typedef BOOL (POOL_DESTRUCTOR_PROC)(
 //
 
 struct POOL_RESOURCE {
-    DWORD created;  // Time when resource was created
+    DWORD created;  // Time when the resource was created
     void *data;     // Opaque data set by the constructor callback
     TAILQ_ENTRY(POOL_RESOURCE) link;
 };
@@ -46,23 +46,23 @@ typedef struct POOL_RESOURCE POOL_RESOURCE;
 // Pool structures
 //
 
-TAILQ_HEAD(POOL_QUEUE, POOL_RESOURCE);
-typedef struct POOL_QUEUE POOL_QUEUE;
+TAILQ_HEAD(POOL_TAIL_QUEUE, POOL_RESOURCE);
+typedef struct POOL_TAIL_QUEUE POOL_TAIL_QUEUE;
 
 typedef struct {
     DWORD                 total;        // Total number of resources
-    DWORD                 idle;         // Number of available resources
+    DWORD                 idle;         // Number of idle resources
     DWORD                 minimum;      // Minimum number of resources to have available
     DWORD                 average;      // Average number of resources to have available
     DWORD                 maximum;      // Maximum number of resources to have available
     DWORD                 expiration;   // Milliseconds until a resource expires
-    DWORD                 timeout;      // Milliseconds to wait for an available resource
+    DWORD                 timeout;      // Milliseconds to wait for a resource to become available
     POOL_CONSTRUCTOR_PROC *constructor; // Procedure called when a resource is created
     POOL_DESTRUCTOR_PROC  *destructor;  // Procedure called when a resource is destroyed
     void                  *opaque;      // Opaque argument passed to the constructor and destructor
-    POOL_QUEUE            availQueue;   // Queue of available resources (populated containers)
-    POOL_QUEUE            freeQueue;    // Queue of free resources (empty containers)
-    CONDITION_VARIABLE    availCond;    // Signaled once a resource becomes available
+    POOL_TAIL_QUEUE       resQueue;     // Queue of resources
+    POOL_TAIL_QUEUE       conQueue;     // Queue of containers
+    CONDITION_VARIABLE    availCond;    // Signaled once a used resource becomes available
     CRITICAL_SECTION      queueLock;    // Synchronize access to the tail queues
 } POOL;
 
