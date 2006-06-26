0a32c0065185688c00c000ef015404e2d0ee7bae
neoxed <neoxed@8dc05064-dfdd-11de-a668-6fd11605f5ce>
2006-06-26 23:49:54 +0000

    Add dynamic string functions.

--- alcotools/trunk/src/dynstring.c
+++ alcotools/trunk/src/dynstring.c
@@ -16,4 +16,192 @@ Abstract:
 
 #include "alcoholicz.h"
 
-// TODO
+apr_status_t
+DsCreate(
+    DYNAMIC_STRING *str,
+    apr_pool_t *pool,
+    const char *data
+    )
+{
+    ASSERT(str != NULL);
+    ASSERT(pool != NULL);
+    ASSERT(data != NULL);
+
+    return DsCreateN(str, pool, data, strlen(data));
+}
+
+apr_status_t
+DsCreateN(
+    DYNAMIC_STRING *str,
+    apr_pool_t *pool,
+    const char *data,
+    apr_size_t length
+    )
+{
+    ASSERT(str != NULL);
+    ASSERT(pool != NULL);
+    ASSERT(data != NULL);
+
+    // TODO
+    return APR_SUCCESS;
+}
+
+apr_status_t
+DsCreateFromFile(
+    DYNAMIC_STRING *str,
+    apr_pool_t *pool,
+    const char *path
+    )
+{
+    ASSERT(str != NULL);
+    ASSERT(pool != NULL);
+    ASSERT(path != NULL);
+
+    // TODO
+    return APR_SUCCESS;
+}
+
+void
+DsDestroy(
+    DYNAMIC_STRING *str
+    )
+{
+    ASSERT(str != NULL);
+    memset(str, 0, sizeof(DYNAMIC_STRING));
+}
+
+
+apr_status_t
+DsAppend(
+    DYNAMIC_STRING *strTarget,
+    DYNAMIC_STRING *strSource
+    )
+{
+    ASSERT(strTarget != NULL);
+    ASSERT(strSource != NULL);
+
+    return DsAppendStrN(strTarget, strSource->data, strSource->length);
+}
+
+apr_status_t
+DsAppendStr(
+    DYNAMIC_STRING *str,
+    const char *data
+    )
+{
+    ASSERT(str != NULL);
+    ASSERT(data != NULL);
+
+    return DsAppendStrN(str, data, strlen(data));
+}
+
+apr_status_t
+DsAppendStrN(
+    DYNAMIC_STRING *str,
+    const char *data,
+    apr_size_t length
+    )
+{
+    apr_size_t total;
+    apr_status_t status;
+
+    ASSERT(str != NULL);
+    ASSERT(data != NULL);
+
+    // Enlarge the buffer, if necessary
+    total = str->length + length;
+    status = DsExpand(str, total + 1);
+    if (status != APR_SUCCESS) {
+        return status;
+    }
+
+    memcpy(str->data + str->length, data, length);
+    str->data[total] = '\0';
+    str->length = total;
+    return APR_SUCCESS;
+}
+
+bool_t
+DsEqual(
+    DYNAMIC_STRING *str1,
+    DYNAMIC_STRING *str2
+    )
+{
+    ASSERT(str1 != NULL);
+    ASSERT(str2 != NULL);
+
+    if (str1->length != str2->length) {
+        return FALSE;
+    }
+
+    // Both strings are of identical lengths
+    return (memcmp(str1->data, str2->data, str1->length) == 0) ? TRUE : FALSE;
+}
+
+apr_status_t
+DsExpand(
+    DYNAMIC_STRING *str,
+    apr_size_t length
+    )
+{
+    apr_size_t newSize;
+    apr_size_t prevSize;
+    char *newData;
+
+    ASSERT(str != NULL);
+
+    // Check if the buffer size is already sufficient
+    if (str->size >= length) {
+        return APR_SUCCESS;
+    }
+
+    if (str->size == 0) {
+        newSize = length;
+    } else {
+        // Continue doubling the current size until we reach the requested length
+        newSize = str->size;
+        do {
+            prevSize = newSize;
+            newSize *= 2;
+
+            // Use the requested length if we overflow
+            if (prevSize > newSize) {
+                newSize = length;
+                break;
+            }
+        } while (newSize < length);
+    }
+
+    // Allocate a larger buffer size
+    newData = apr_palloc(str->pool, newSize);
+    if (newData == NULL) {
+        return APR_ENOMEM;
+    }
+
+    // Copy the current data to the new buffer. Unfortunately, we have to leak
+    // the previous block since there's no way to free pool memory until the
+    // pool is cleared or destroyed (i.e. need a apr_prealloc/apr_pfree function).
+    memcpy(newData, str->data, str->size);
+
+    // Update the string structure
+    str->data = newData;
+    str->size = newSize;
+    return APR_SUCCESS;
+}
+
+apr_status_t
+DsTruncate(
+    DYNAMIC_STRING *str,
+    apr_size_t length
+    )
+{
+    ASSERT(str != NULL);
+
+    if (length > str->length) {
+        return APR_EINVAL;
+    }
+
+    str->length = length;
+    str->data[length] = '\0';
+    return APR_SUCCESS;
+}
--- alcotools/trunk/src/dynstring.h
+++ alcotools/trunk/src/dynstring.h
@@ -18,95 +18,89 @@ Abstract:
 #define _DYNSTRING_H_
 
 typedef struct {
-    char    *value;     // Pointer to a null terminated string.
-    size_t  length;     // Length of string in "value", in characters (without the null).
-    size_t  maxLength;  // Size of the buffer pointed to by "value", in bytes.
-} DynString;
+    char       *data;   // Pointer to the string
+    apr_pool_t *pool;   // Pointer to the pool the string is allocated from
+    apr_size_t length;  // Length of the string, in characters
+    apr_size_t size;    // Size of the buffer allocated, in bytes
+} DYNAMIC_STRING;
 
 //
-// Macros for accessing members of the "DynString" structure.
+// Macros for accessing members of the DYNAMIC_STRING structure.
 //
 
-#define DynStringGet(dynStr)    ((dynStr)->value)
-#define DynStringLength(dynStr) ((dynStr)->length)
+#define DsGetData(str)    ((str)->data)
+#define DsGetLength(str)  ((str)->length)
 
 //
 // Functions for creating and deleting dynamic strings.
 //
 
-DynString *
-DynStringCreate(
-    const char *value
+apr_status_t
+DsCreate(
+    DYNAMIC_STRING *str,
+    apr_pool_t *pool,
+    const char *data
     );
 
-DynString *
-DynStringCreateN(
-    const char *value,
-    size_t length
+apr_status_t
+DsCreateN(
+    DYNAMIC_STRING *str,
+    apr_pool_t *pool,
+    const char *data,
+    apr_size_t length
     );
 
-DynString *
-DynStringFromFile(
+apr_status_t
+DsCreateFromFile(
+    DYNAMIC_STRING *str,
+    apr_pool_t *pool,
     const char *path
     );
 
 void
-DynStringDestroy(
-    DynString *dynStr
+DsDestroy(
+    DYNAMIC_STRING *str
     );
 
 //
 // Functions for manipulating dynamic strings.
 //
 
-int
-DynStringAppend(
-    DynString *dynStr,
-    const char *value
+apr_status_t
+DsAppend(
+    DYNAMIC_STRING *strTarget,
+    DYNAMIC_STRING *strSource
     );
 
-int
-DynStringAppendN(
-    DynString *dynStr,
-    const char *value,
-    size_t length
+apr_status_t
+DsAppendStr(
+    DYNAMIC_STRING *str,
+    const char *data
     );
 
-DynString *
-DynStringDup(
-    DynString *dynStr
+apr_status_t
+DsAppendStrN(
+    DYNAMIC_STRING *str,
+    const char *data,
+    apr_size_t length
     );
 
-int
-DynStringEqual(
-    DynString *dynStr1,
-    DynString *dynStr2
+bool_t
+DsEqual(
+    DYNAMIC_STRING *str1,
+    DYNAMIC_STRING *str2
     );
 
-int
-DynStringFormat(
-    DynString *dynStr,
-    const char *format,
-    ...
+apr_status_t
+DsExpand(
+    DYNAMIC_STRING *str,
+    apr_size_t length
     );
 
-int
-DynStringFormatV(
-    DynString *dynStr,
-    const char *format,
-    va_list argList
-    );
-
-int
-DynStringSet(
-    DynString *dynStr,
-    const char *value
-    );
-
-int
-DynStringTruncate(
-    DynString *dynStr,
-    size_t length
+apr_status_t
+DsTruncate(
+    DYNAMIC_STRING *str,
+    apr_size_t length
     );
 
 #endif // _DYNSTRING_H_
