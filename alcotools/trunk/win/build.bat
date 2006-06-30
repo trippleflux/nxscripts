4e3628a1495dff401f25c5df74c23637ed1f39d4
neoxed <neoxed@8dc05064-dfdd-11de-a668-6fd11605f5ce>
2006-06-30 01:28:05 +0000

    Update build scripts.

--- alcotools/trunk/win/build.bat
+++ alcotools/trunk/win/build.bat
@@ -1,8 +1,10 @@
 @ECHO OFF
 CALL "%VS80COMNTOOLS%\vsvars32.bat"
-SET APR_DIR=E:\Projects\Libraries\APR-1.2.7
-SET SQLITE_DIR=E:\Projects\Libraries\SQLite-3.3.6
-SET ZLIB_DIR=E:\Projects\Libraries\Zlib-1.2.3
+SET APR_DIR=E:\Projects\Libraries\StaticCRT\APR-1.2.7
+SET SQLITE_DIR=E:\Projects\Libraries\StaticCRT\SQLite-3.3.6
+SET ZLIB_DIR=E:\Projects\Libraries\StaticCRT\Zlib-1.2.3
+SET STATIC_CRT=1
+SET STATIC_LIB=1
 
 ECHO:=======================================================================
 ECHO: AlcoTools - Release Build
@@ -18,10 +20,12 @@ ECHO:=======================================================================
 nmake -nologo -f makefile.win DEBUG=1 MEMDEBUG=1
 IF errorlevel 1 GOTO error
 
+ECHO:=======================================================================
 ECHO *** Finished ***
 GOTO end
 
 :error
+ECHO:=======================================================================
 ECHO *** Error ***
 
 :end
--- alcotools/trunk/win/clean.bat
+++ alcotools/trunk/win/clean.bat
@@ -1,8 +1,10 @@
 @ECHO OFF
 CALL "%VS80COMNTOOLS%\vsvars32.bat"
-SET APR_DIR=E:\Projects\Libraries\APR-1.2.7
-SET SQLITE_DIR=E:\Projects\Libraries\SQLite-3.3.6
-SET ZLIB_DIR=E:\Projects\Libraries\Zlib-1.2.3
+SET APR_DIR=E:\Projects\Libraries\StaticCRT\APR-1.2.7
+SET SQLITE_DIR=E:\Projects\Libraries\StaticCRT\SQLite-3.3.6
+SET ZLIB_DIR=E:\Projects\Libraries\StaticCRT\Zlib-1.2.3
+SET STATIC_CRT=1
+SET STATIC_LIB=1
 
 ECHO:========================================================================
 ECHO: AlcoTools - Cleaning
