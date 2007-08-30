de714185d915240856dd0d4db0f9dd6263cabf03
neoxed <neoxed@8dc05064-dfdd-11de-a668-6fd11605f5ce>
2007-08-30 07:12:38 +0000

    First draft of a locking implementation using UUIDs and expirations.

--- nxmydb/trunk/source/userdb.c
+++ nxmydb/trunk/source/userdb.c
@@ -49,20 +49,144 @@ DWORD DbUserDelete(DB_CONTEXT *db, CHAR *userName)
 
 DWORD DbUserLock(DB_CONTEXT *db, CHAR *userName)
 {
+    CHAR        *lockOwner;
+    INT         lockExpire;
+    INT         lockTimeout;
+    CHAR        *query;
+    INT         result;
+    INT64       affectedRows;
+    MYSQL_BIND  binds[3];
+
     ASSERT(db != NULL);
     ASSERT(userName != NULL);
     TRACE("db=%p userName=%s\n", db, userName);
 
-    return ERROR_INTERNAL_ERROR;
+    DbGetConfig(&lockExpire, &lockTimeout, &lockOwner);
+
+    //
+    // Prepate statement
+    //
+
+    // TODO: lockTimeout
+
+    query = "UPDATE io_users SET lockowner=?, locktime=UNIX_TIMESTAMP()"
+            "  WHERE name=?"
+            "    AND lockowner IS NULL OR (UNIX_TIMESTAMP() - locktime) > ?";
+
+    result = mysql_stmt_prepare(db->stmt, query, strlen(query));
+    if (result != 0) {
+        TRACE("Unable to prepare statement: %s\n", mysql_stmt_error(db->stmt));
+        return DbMapError(result);
+    }
+
+    //
+    // Bind parameters
+    //
+
+    DB_CHECK_BINDS(binds, db);
+    ZeroMemory(&binds, sizeof(binds));
+
+    binds[0].buffer_type   = MYSQL_TYPE_STRING;
+    binds[0].buffer        = lockOwner;
+    binds[0].buffer_length = strlen(lockOwner);
+
+    binds[1].buffer_type   = MYSQL_TYPE_STRING;
+    binds[1].buffer        = userName;
+    binds[1].buffer_length = strlen(userName);
+
+    binds[2].buffer_type   = MYSQL_TYPE_LONG;
+    binds[2].buffer        = &lockExpire;
+
+    result = mysql_stmt_bind_param(db->stmt, binds);
+    if (result != 0) {
+        TRACE("Unable to bind parameters: %s\n", mysql_stmt_error(db->stmt));
+        return DbMapError(result);
+    }
+
+    //
+    // Execute statement
+    //
+
+    result = mysql_stmt_execute(db->stmt);
+    if (result != 0) {
+        TRACE("Unable to execute statement: %s\n", mysql_stmt_error(db->stmt));
+        return DbMapError(result);
+    }
+
+    affectedRows = mysql_stmt_affected_rows(db->stmt);
+    if (affectedRows == 0) {
+        TRACE("Unable to lock user, no affected rows.\n");
+        return ERROR_LOCKED;
+    }
+
+    return ERROR_SUCCESS;
 }
 
 DWORD DbUserUnlock(DB_CONTEXT *db, CHAR *userName)
 {
+    CHAR        *lockOwner;
+    CHAR        *query;
+    INT         result;
+    INT64       affectedRows;
+    MYSQL_BIND  binds[2];
+
     ASSERT(db != NULL);
     ASSERT(userName != NULL);
     TRACE("db=%p userName=%s\n", db, userName);
 
-    return ERROR_INTERNAL_ERROR;
+    DbGetConfig(NULL, NULL, &lockOwner);
+
+    //
+    // Prepate statement
+    //
+
+    query = "UPDATE io_users SET lockowner=NULL, locktime=0"
+            "  WHERE name=? AND lockowner=?";
+
+    result = mysql_stmt_prepare(db->stmt, query, strlen(query));
+    if (result != 0) {
+        TRACE("Unable to prepare statement: %s\n", mysql_stmt_error(db->stmt));
+        return DbMapError(result);
+    }
+
+    //
+    // Bind parameters
+    //
+
+    DB_CHECK_BINDS(binds, db);
+    ZeroMemory(&binds, sizeof(binds));
+
+    binds[0].buffer_type   = MYSQL_TYPE_STRING;
+    binds[0].buffer        = userName;
+    binds[0].buffer_length = strlen(userName);
+
+    binds[1].buffer_type   = MYSQL_TYPE_STRING;
+    binds[1].buffer        = lockOwner;
+    binds[1].buffer_length = strlen(lockOwner);
+
+    result = mysql_stmt_bind_param(db->stmt, binds);
+    if (result != 0) {
+        TRACE("Unable to bind parameters: %s\n", mysql_stmt_error(db->stmt));
+        return DbMapError(result);
+    }
+
+    //
+    // Execute statement
+    //
+
+    result = mysql_stmt_execute(db->stmt);
+    if (result != 0) {
+        TRACE("Unable to execute statement: %s\n", mysql_stmt_error(db->stmt));
+        return DbMapError(result);
+    }
+
+    affectedRows = mysql_stmt_affected_rows(db->stmt);
+    if (affectedRows == 0) {
+        // Failure is acceptable at this point.
+        TRACE("Unable to unlock user, no affected rows.\n");
+    }
+
+    return ERROR_SUCCESS;
 }
 
 DWORD DbUserOpen(DB_CONTEXT *db, CHAR *userName, USERFILE *userFile)
@@ -96,8 +220,6 @@ DWORD DbUserRefresh(DB_CONTEXT *db)
 {
     ASSERT(db != NULL);
     TRACE("db=%p\n", db);
-
-    DbUserLock(db, "test");
 
     return ERROR_INTERNAL_ERROR;
 }
