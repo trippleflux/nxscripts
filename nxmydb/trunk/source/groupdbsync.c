c334deb5b9d8583111f4a3aab1d7a044c9d8e9df
neoxed <neoxed@8dc05064-dfdd-11de-a668-6fd11605f5ce>
2007-09-20 06:16:43 +0000

    Refresh timer:  * Implement the logic required for full syncs.  * The handles for creating, renaming, deleting and updating still need to be written.

--- nxmydb/trunk/source/groupdbsync.c
+++ nxmydb/trunk/source/groupdbsync.c
@@ -17,36 +17,136 @@ Abstract:
 #include <base.h>
 #include <backends.h>
 #include <database.h>
-#include <idlist.h>
+#include <namelist.h>
 
 static DWORD SyncFull(DB_CONTEXT *db)
 {
-    DWORD   result;
-    ID_LIST list;
+    CHAR        *query;
+    CHAR        groupName[_MAX_NAME + 1];
+    DWORD       error;
+    DWORD       i;
+    GROUPFILE   groupFile;
+    INT         result;
+    NAME_ENTRY  *entry;
+    NAME_LIST   list;
+    ULONG       outputLength;
+    MYSQL_BIND  bind[5];
+    MYSQL_RES   *metadata;
+    MYSQL_STMT  *stmt;
 
     ASSERT(db != NULL);
 
-    result = IdListCreate(&list, ID_LIST_TYPE_GROUP);
-    if (result != ERROR_SUCCESS) {
-        TRACE("Unable to create group ID list (error %lu).\n", result);
-        return result;
+    //
+    // Build list of group IDs
+    //
+
+    error = NameListCreateGroups(&list);
+    if (error != ERROR_SUCCESS) {
+        TRACE("Unable to create group ID list (error %lu).\n", error);
+        return error;
     }
 
     //
-    // TODO
-    //
-    // process io_group table
-    //  - select all data from the groups table
-    //  - resolve group name to ID
-    //  - look up group ID in the hash table and delete it
-    //    - if group exists, update group
-    //    - if group does not exist, register group
-    //
-    // process hash table
-    //  - delete groups remaining in the hash table
+    // Prepare and execute statement
     //
 
-    IdListDestroy(&list);
+    stmt = db->stmt[7];
+
+    query = "SELECT name, description, slots, users, vfsfile FROM io_group";
+
+    result = mysql_stmt_prepare(stmt, query, strlen(query));
+    if (result != 0) {
+        TRACE("Unable to prepare statement: %s\n", mysql_stmt_error(stmt));
+        return DbMapErrorFromStmt(stmt);
+    }
+
+    metadata = mysql_stmt_result_metadata(stmt);
+    if (metadata == NULL) {
+        TRACE("Unable to prepare statement: %s\n", mysql_stmt_error(stmt));
+        return DbMapErrorFromStmt(stmt);
+    }
+
+    result = mysql_stmt_execute(stmt);
+    if (result != 0) {
+        TRACE("Unable to execute statement: %s\n", mysql_stmt_error(stmt));
+        return DbMapErrorFromStmt(stmt);
+    }
+
+    //
+    // Bind and fetch results
+    //
+
+    DB_CHECK_RESULTS(bind, metadata);
+    ZeroMemory(&bind, sizeof(bind));
+
+    bind[0].buffer_type   = MYSQL_TYPE_STRING;
+    bind[0].buffer        = groupName;
+    bind[0].buffer_length = sizeof(groupName);
+    bind[0].length        = &outputLength;
+
+    bind[1].buffer_type   = MYSQL_TYPE_STRING;
+    bind[1].buffer        = groupFile.szDescription;
+    bind[1].buffer_length = sizeof(groupFile.szDescription);
+    bind[1].length        = &outputLength;
+
+    bind[2].buffer_type   = MYSQL_TYPE_BLOB;
+    bind[2].buffer        = groupFile.Slots;
+    bind[2].buffer_length = sizeof(groupFile.Slots);
+    bind[2].length        = &outputLength;
+
+    bind[3].buffer_type   = MYSQL_TYPE_LONG;
+    bind[3].buffer        = &groupFile.Users;
+
+    bind[4].buffer_type   = MYSQL_TYPE_STRING;
+    bind[4].buffer        = groupFile.szVfsFile;
+    bind[4].buffer_length = sizeof(groupFile.szVfsFile);
+    bind[4].length        = &outputLength;
+
+    result = mysql_stmt_bind_result(stmt, bind);
+    if (result != 0) {
+        TRACE("Unable to bind results: %s\n", mysql_stmt_error(stmt));
+        return DbMapErrorFromStmt(stmt);
+    }
+
+    result = mysql_stmt_store_result(stmt);
+    if (result != 0) {
+        TRACE("Unable to buffer results: %s\n", mysql_stmt_error(stmt));
+        return DbMapErrorFromStmt(stmt);
+    }
+
+    for (;;) {
+        ZeroMemory(&groupFile, sizeof(GROUPFILE));
+        if (mysql_stmt_fetch(stmt) != 0) {
+            break;
+        }
+
+        if (!NameListRemove(&list, groupName)) {
+            // Group does not exist, create it.
+
+            // TODO
+            TRACE("GrpCrt %s\n", groupName);
+        } else {
+            // Group already exists, update it.
+
+            // TODO
+            TRACE("GrpUpd %s\n", groupName);
+        }
+    }
+
+    mysql_free_result(metadata);
+
+    //
+    // Delete remaining groups
+    //
+
+    for (i = 0; i < list.count; i++) {
+        entry = list.array[i];
+
+        // TODO
+        TRACE("GrpDel %s (%d)\n", entry->name, entry->id);
+    }
+
+    NameListDestroy(&list);
 
     return ERROR_NOT_SUPPORTED;
 }
