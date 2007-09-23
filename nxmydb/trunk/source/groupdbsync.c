8c7adf1db68afab43514cd63e3fe8bfae2c41a44
neoxed <neoxed@8dc05064-dfdd-11de-a668-6fd11605f5ce>
2007-09-23 03:13:08 +0000

    Refresh timer:  * Implement sync event functions (create, rename, update, and delete).  * Fully implement the full sync function.

--- nxmydb/trunk/source/groupdbsync.c
+++ nxmydb/trunk/source/groupdbsync.c
@@ -19,7 +19,99 @@ Abstract:
 #include <database.h>
 #include <namelist.h>
 
-static DWORD GroupSyncFull(DB_CONTEXT *db)
+static DWORD EventCreate(CHAR *groupName, GROUPFILE *groupFile)
+{
+    MOD_CONTEXT *mod;
+    DWORD       result;
+    INT32       groupId;
+
+    ASSERT(groupName != NULL);
+    ASSERT(groupFile != NULL);
+
+    // Module context is required for all file operations
+    mod = Io_Allocate(sizeof(MOD_CONTEXT));
+    if (mod == NULL) {
+        result = ERROR_NOT_ENOUGH_MEMORY;
+        TRACE("Unable to allocate module context.\n");
+
+    } else {
+        // Initialize MOD_CONTEXT structure
+        mod->file = INVALID_HANDLE_VALUE;
+        groupFile->lpInternal = mod;
+
+        // Register group
+        result = GroupRegister(groupName, groupFile, &groupId);
+        if (result != ERROR_SUCCESS) {
+            TRACE("Unable to register group (error %lu).\n", result);
+        } else {
+
+            // Create group file
+            result = FileGroupCreate(groupId, groupFile);
+            if (result != ERROR_SUCCESS) {
+                TRACE("Unable to create group file (error %lu).\n", result);
+
+                //Creation failed, clean-up the group file
+                FileGroupDelete(groupId);
+                FileGroupClose(groupFile);
+                GroupUnregister(groupName);
+            }
+        }
+
+        if (result != ERROR_SUCCESS) {
+            // Free module context after all file operations
+            Io_Free(mod);
+        }
+    }
+
+    return result;
+}
+
+static DWORD EventRename(CHAR *groupName, CHAR *newName)
+{
+    DWORD result;
+
+    ASSERT(groupName != NULL);
+    ASSERT(newName != NULL);
+
+    // Register group under the new name
+    result = GroupRegisterAs(groupName, newName);
+
+    return result;
+}
+
+static DWORD EventDelete(CHAR *groupName, INT32 groupId)
+{
+    DWORD result;
+
+    ASSERT(groupName != NULL);
+
+    // Delete group file (success does not matter)
+    result = FileGroupDelete(groupId);
+    if (result != ERROR_SUCCESS) {
+        TRACE("Unable to delete group file (error %lu).\n", result);
+    }
+
+    // Unregister group
+    result = GroupUnregister(groupName);
+
+    return result;
+}
+
+static DWORD EventUpdate(CHAR *groupName, GROUPFILE *groupFile)
+{
+    DWORD result;
+
+    ASSERT(groupName != NULL);
+    ASSERT(groupFile != NULL);
+
+    // Update the group file
+    result = GroupUpdateByName(groupName, groupFile);
+
+    return result;
+}
+
+
+static DWORD SyncFull(DB_CONTEXT *db)
 {
     CHAR        *query;
     CHAR        groupName[_MAX_NAME + 1];
@@ -121,15 +213,21 @@ static DWORD GroupSyncFull(DB_CONTEXT *db)
         }
 
         if (!NameListRemove(&list, groupName)) {
-            // Group does not exist, create it.
+            TRACE("GroupSyncFull: Create(%s)\n", groupName);
 
-            // TODO
-            TRACE("GrpCrt %s\n", groupName);
+            // Group does not exist locally, create it.
+            error = EventCreate(groupName, &groupFile);
+            if (error != ERROR_SUCCESS) {
+                TRACE("Unable to create group \"%s\" (error %lu).\n", groupName, error);
+            }
         } else {
-            // Group already exists, update it.
+            TRACE("GroupSyncFull: Update(%s)\n", groupName);
 
-            // TODO
-            TRACE("GrpUpd %s\n", groupName);
+            // Group already exists locally, update it.
+            error = EventUpdate(groupName, &groupFile);
+            if (error != ERROR_SUCCESS) {
+                TRACE("Unable to update group \"%s\" (error %lu).\n", groupName, error);
+            }
         }
     }
 
@@ -141,9 +239,13 @@ static DWORD GroupSyncFull(DB_CONTEXT *db)
 
     for (i = 0; i < list.count; i++) {
         entry = list.array[i];
+        TRACE("GroupSyncFull: Delete(%s,%d)\n", entry->name, entry->id);
 
-        // TODO
-        TRACE("GrpDel %s (%d)\n", entry->name, entry->id);
+        // Group does not exist on database, delete it.
+        error = EventDelete(entry->name, entry->id);
+        if (error != ERROR_SUCCESS) {
+            TRACE("Unable to delete group \"%s\" (error %lu).\n", entry->name, error);
+        }
     }
 
     NameListDestroy(&list);
@@ -151,7 +253,7 @@ static DWORD GroupSyncFull(DB_CONTEXT *db)
     return ERROR_SUCCESS;
 }
 
-static DWORD GroupSyncIncremental(DB_CONTEXT *db, SYNC_CONTEXT *sync)
+static DWORD SyncIncremental(DB_CONTEXT *db, SYNC_CONTEXT *sync)
 {
     ASSERT(db != NULL);
     ASSERT(sync != NULL);
@@ -169,6 +271,7 @@ static DWORD GroupSyncIncremental(DB_CONTEXT *db, SYNC_CONTEXT *sync)
     return ERROR_SUCCESS;
 }
 
+
 DWORD DbGroupSync(DB_CONTEXT *db, SYNC_CONTEXT *sync)
 {
     DWORD result;
@@ -180,10 +283,10 @@ DWORD DbGroupSync(DB_CONTEXT *db, SYNC_CONTEXT *sync)
     if (sync->prevUpdate == 0) {
         // If there was no previous update time, we
         // perform a full group syncronization.
-        result = GroupSyncFull(db);
+        result = SyncFull(db);
     } else {
         ASSERT(sync->currUpdate != 0);
-        result = GroupSyncIncremental(db, sync);
+        result = SyncIncremental(db, sync);
     }
 
     return result;
