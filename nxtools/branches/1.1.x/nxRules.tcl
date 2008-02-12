f801506f90e18ef956700657ae8a8ad237829e03
neoxed <neoxed@8dc05064-dfdd-11de-a668-6fd11605f5ce>
2008-02-12 02:04:58 +0000

    Branching the stable 1.1 version.

---
+++ nxtools/branches/1.1.x/nxRules.tcl
@@ -0,0 +1,88 @@
+#
+# nxTools - Dupe Checker, nuker, pre, request and utilities.
+# Copyright (c) 2004-2008 neoxed
+#
+# Module Name:
+#   Rules
+#
+# Author:
+#   neoxed (neoxed@gmail.com)
+#
+# Abstract:
+#   Implements a rules script.
+#
+
+if {[IsTrue $misc(ReloadConfig)] && [catch {source "../scripts/init.itcl"} error]} {
+    iputs "Unable to load script configuration, contact a siteop."
+    return -code error $error
+}
+
+namespace eval ::nxTools::Rules {
+    namespace import -force ::nxLib::*
+}
+
+# Rules Main
+######################################################################
+
+proc ::nxTools::Rules::Main {display} {
+    global misc rules
+    if {[IsTrue $misc(DebugMode)]} {DebugLog -state [info script]}
+    set displayList [list]
+    set sectionList [list]
+    set sectionName ""
+
+    # Read rules configuration.
+    if {![catch {set handle [open $rules(ConfigFile) r]} error]} {
+        while {![eof $handle]} {
+            set line [string trim [gets $handle]]
+            if {![string length $line] || [string index $line 0] eq "#"} {continue}
+
+            if {[string match {\[*\]} $line]} {
+                set sectionName [string range $line 1 end-1]
+                lappend sectionList $sectionName
+            } elseif {[set index [string first "|" $line]] != -1} {
+                lappend rule($sectionName) [string range $line 0 [incr index -1]] [string range $line [incr index 2] end]
+            }
+        }
+        close $handle
+    } else {ErrorLog RulesRead $error; return 1}
+
+    # Find the specified section, display all areas if is there no match.
+    foreach sectionName $sectionList {
+        if {[string equal -nocase $sectionName $display]} {
+            set displayList [list $sectionName]; break
+        }
+    }
+    if {![string length $displayList]} {set displayList $sectionList}
+
+    foreach fileExt {Header Section SingleLine MultiLine Footer} {
+        set template($fileExt) [ReadFile [file join $misc(Templates) "Rules.$fileExt"]]
+    }
+    OutputText [ParseCookies $template(Header) [list $sectionList] {sections}]
+
+    foreach sectionName $displayList {
+        set ruleList [lindex [array get rule $sectionName] 1]
+        if {![llength $ruleList]} {continue}
+        set count 0
+        OutputText [ParseCookies $template(Section) [list $sectionName $sectionList] {section sections}]
+
+        foreach {punishment text} $ruleList {
+            incr count; set isMultiLine 0
+
+            # Wrap each line before displaying.
+            foreach line [WordWrap $text $rules(LineWidth)] {
+                set valueList [list $count $punishment $line $sectionName]
+                if {!$isMultiLine} {
+                    OutputText [ParseCookies $template(SingleLine) $valueList {num punishment rule section}]
+                } else {
+                    OutputText [ParseCookies $template(MultiLine) $valueList {num punishment rule section}]
+                }
+                incr isMultiLine
+            }
+        }
+    }
+    OutputText [ParseCookies $template(Footer) [list $sectionList] {sections}]
+    return 0
+}
+
+::nxTools::Rules::Main [expr {[info exists args] ? $args : ""}]
